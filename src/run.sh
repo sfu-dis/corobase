fd83e96ef6c0186c8f52afae0f65c616cf3bea6d
Kangnyeon Kim <knkim@cs.toronto.edu>
2014-09-15 23:19:41 -0400

    32 threads work fine without release_no_rcu

--- src/run.sh
+++ src/run.sh
@@ -1,4 +1,4 @@
-rm -rf /run/silo-log/* 
+rm -rf /tmpfs/silo-log/* 
 export LD_PRELOAD="/usr/lib/libtcmalloc.so" 
-./out-perf.debug.check.masstree/benchmarks/dbtest --verbose --bench tpcc --scale-factor 2 --num-threads 4 --runtime 20 --log-dir /run/silo-log/
+./out-perf.debug.check.masstree/benchmarks/dbtest --verbose --bench tpcc --scale-factor 2 --num-threads 4 --runtime 20 --log-dir /tmpfs/silo-log/
 
