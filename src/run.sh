b5bb75fb6c68ccbd3fa6d37617d0618b058e252f
Kangnyeon Kim <knkim@cs.toronto.edu>
2014-10-22 00:06:38 -0400

    per socket OID alloc, ugly code

--- src/run.sh
+++ src/run.sh
@@ -1,8 +1,8 @@
 rm -rf /tmpfs/silo-log/*
 export LD_PRELOAD="/usr/lib/libtcmalloc.so"
 
-CPU_RANGE="0-`expr $1 + 1`"			# 2 Background processes
+CPU_RANGE="24-`expr $1 + 1 + 24`"			# 2 Background processes
 echo $CPU_RANGE
 taskset -c "$CPU_RANGE" ./out-perf.masstree/benchmarks/dbtest --verbose --bench tpcc --scale-factor $1 --num-threads $1 --runtime 20 --log-dir /tmpfs/silo-log/
-#./out-perf.debug.check.masstree/benchmarks/dbtest --verbose --bench tpcc --scale-factor $1 --num-threads $1 --runtime 30 --log-dir /tmpfs/silo-log/
+#./out-perf.debug.check.masstree/benchmarks/dbtest --verbose --bench tpcc --scale-factor $1 --num-threads $1 --runtime 20 --log-dir /tmpfs/silo-log/
 
