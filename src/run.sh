6c8effbcd60ea6ae0c68802c1b6015fa3bcbb617
Ryan <ryan.johnson@cs.utoronto.ca>
2014-10-22 21:57:46 -0400

    Tweak runtime script to always create 24WH, run 15s instead of 10

--- src/run.sh
+++ src/run.sh
@@ -3,6 +3,6 @@ export LD_PRELOAD="/usr/lib/libtcmalloc.so"
 
 CPU_RANGE="0-`expr $1 + 1`"			# 2 Background processes
 echo $CPU_RANGE
-taskset -c "$CPU_RANGE" ./out-perf.masstree/benchmarks/dbtest --verbose --bench tpcc --scale-factor $1 --num-threads $1 --runtime 10 --log-dir /tmpfs/silo-log/
+taskset -c "$CPU_RANGE" ./out-perf.masstree/benchmarks/dbtest --verbose --bench tpcc --scale-factor 24 --num-threads $1 --runtime 15 --log-dir /tmpfs/silo-log/
 #./out-perf.debug.check.masstree/benchmarks/dbtest --verbose --bench tpcc --scale-factor $1 --num-threads $1 --runtime 20 --log-dir /tmpfs/silo-log/
 
