c79dbe1d216529c44e27d4d622df31976ec17be5
Tianzheng Wang <tzwang@cs.toronto.edu>
2014-10-24 22:09:11 -0400

    move stale tuples to cold store during gc; use malloc for tree nodes

--- src/run.sh
+++ src/run.sh
@@ -4,5 +4,5 @@ export LD_PRELOAD="/usr/lib/libtcmalloc.so"
 CPU_RANGE="0-`expr $1 + 4`"			# 2 Background processes
 #echo $CPU_RANGE
 taskset -c "$CPU_RANGE" ./out-perf.masstree/benchmarks/dbtest --verbose --bench tpcc --scale-factor 24 --num-threads $1 --runtime 30 --log-dir /tmpfs/silo-log/
-#./out-perf.debug.check.masstree/benchmarks/dbtest --verbose --bench tpcc --scale-factor $1 --num-threads $1 --runtime 20 --log-dir /tmpfs/silo-log/
+#taskset -c "$CPU_RANGE" ./out-perf.debug.check.masstree/benchmarks/dbtest --verbose --bench tpcc --scale-factor 24 --num-threads $1 --runtime 30 --log-dir /tmpfs/silo-log/
 
