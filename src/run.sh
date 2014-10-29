6907bd4a7f0448378143469f1da1c0b7eb1f047e
Tianzheng Wang <tzwang@cs.toronto.edu>
2014-10-29 13:37:27 -0400

    use 24 wh in run.sh.

--- src/run.sh
+++ src/run.sh
@@ -5,7 +5,7 @@ else
 	echo "Available! :) "
 	rm -rf /tmpfs/silo-log/*
 	export LD_PRELOAD="/usr/lib/libtcmalloc.so"
-	./out-perf.masstree/benchmarks/dbtest --verbose --bench tpcc --scale-factor 12 --num-threads $1 --runtime 120 --log-dir /tmpfs/silo-log/ --pin-cpu
+	./out-perf.masstree/benchmarks/dbtest --verbose --bench tpcc --scale-factor 24 --num-threads $1 --runtime 120 --log-dir /tmpfs/silo-log/ --pin-cpu
 #	./out-perf.debug.check.masstree/benchmarks/dbtest --verbose --bench tpcc --scale-factor 12 --num-threads $1 --runtime 60 --log-dir /tmpfs/silo-log/ --pin-cpu
 	rm -rf /tmpfs/silo-log/*
 fi
