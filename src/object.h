dc0a6a0698351877ff530a0743ca4e2c4a3cd171
Tianzheng Wang <tzwang@cs.toronto.edu>
2014-10-29 12:48:42 -0400

    extent based oid allocator.

--- src/object.h
+++ src/object.h
@@ -10,6 +10,10 @@
 #include <limits.h>
 
 #define NR_SOCKETS 4
+// each socket requests this many oids a time from global alloc
+#define OID_EXT_BITS 10
+#define OID_EXT_SIZE ((uint64_t){1} << OID_EXT_BITS)
+
 typedef unsigned long long oid_type;
 
 class object
@@ -28,27 +32,16 @@ class object_vector
 public:
 	inline unsigned long long size() 
 	{
-		auto max = 0;
-		for( auto i = 0; i < NR_SOCKETS; i++ )
-		{
-			if( max < *_alloc_offset[i] )
-				max = *_alloc_offset[i];
-		}
-		return (max + 1) * NR_SOCKETS;
-	}
-
-    inline unsigned long long size(int skt)
-    {
-        return *_alloc_offset[skt];
+		return _global_oid_alloc_offset + 1;
     }
 
 	object_vector( unsigned long long nelems)
 	{
-		for( int i = 0; i < NR_SOCKETS; i ++ )
-		{
-			_alloc_offset[i] = (unsigned int*)numa_alloc_onnode( sizeof( unsigned int ), i );
-			*_alloc_offset[i] = 0;
-		}
+        for (int i = 0; i < NR_SOCKETS; i++) {
+            _local_oid_alloc_offset[i] = OID_EXT_SIZE * i;
+            _local_oid_allocated[i] = 0;
+        }
+        _global_oid_alloc_offset = OID_EXT_SIZE * NR_SOCKETS;
 		_obj_table = dynarray<object*>(  std::numeric_limits<unsigned int>::max(), nelems*sizeof(object*) );
 		_obj_table.sanitize( 0,  nelems* sizeof(object*));
 	}
@@ -78,7 +71,7 @@ public:
 
 	inline object* begin( oid_type oid )
 	{
-        ASSERT(oid < size());
+        ASSERT(oid <= size());
 		return volatile_read(_obj_table[oid]);
 	}
 
@@ -116,12 +109,24 @@ retry:
 
 	inline oid_type alloc()
 	{
-		int cpu = sched_getcpu();
-		ALWAYS_ASSERT(cpu >= 0 );
-		int numa_node = cpu % NR_SOCKETS;		// by current topology
-		_obj_table.ensure_size( sizeof(object*) * NR_SOCKETS );
-		auto local_offset = __sync_add_and_fetch( _alloc_offset[numa_node], 1 );		// on NUMA node CAS
-		return local_offset * NR_SOCKETS + numa_node;
+		int node = sched_getcpu() % NR_SOCKETS;
+retry:
+        uint64_t nallocated = __sync_add_and_fetch(&_local_oid_allocated[node], 1);
+        uint64_t overflow = nallocated >> OID_EXT_BITS;
+        if (overflow == 1) {
+            volatile_write(_local_oid_alloc_offset[node], alloc_oid_extent(node));
+            __sync_fetch_and_sub(&_local_oid_allocated[node], OID_EXT_SIZE);
+            goto retry;
+        }
+        else if (overflow > 1)
+            goto retry;
+        return _local_oid_alloc_offset[node] + nallocated; // oid starts at 1
+    }
+
+    inline uint64_t alloc_oid_extent(int socket) {
+		uint64_t noffset = __sync_fetch_and_add(&_global_oid_alloc_offset, OID_EXT_SIZE);
+		_obj_table.ensure_size(sizeof(object*) * (_global_oid_alloc_offset + 1));
+        return noffset;
 	}
 
 	inline void dealloc(object* desc)
@@ -130,7 +135,8 @@ retry:
 	}
 
 private:
-	dynarray<object*> 		_obj_table;
-	unsigned int* _alloc_offset[NR_SOCKETS];
-
+    dynarray<object*> _obj_table;
+    uint64_t _local_oid_alloc_offset[NR_SOCKETS];
+    uint64_t _local_oid_allocated[NR_SOCKETS];
+    uint64_t _global_oid_alloc_offset;
 };
