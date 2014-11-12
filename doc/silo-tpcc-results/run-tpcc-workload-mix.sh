53a2f9b83a98727a257515a578fbc82ac5ed0926
Tianzheng Wang <tzwang@cs.toronto.edu>
2014-11-12 12:49:17 -0500

    change scripts and results for running silo tpcc with varying tx freqs.

--- doc/silo-tpcc-results/run-tpcc-workload-mix.sh
+++ doc/silo-tpcc-results/run-tpcc-workload-mix.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
-# $1 - % of new order
-# $2 - % of payment
-# $3 - % of delivery
-# $4 - % of order status
-# $5 - $ of stock level
+# $1 - silo exec
 
-# $6 - sf/threads
-# $7 - output dir
+# $2 - % of new order
+# $3 - % of payment
+# $4 - % of delivery
+# $5 - % of order status
+# $6 - $ of stock level
+
+# $7 - sf/threads
+# $8 - output dir
+
 
 # workload mix:
 # 12, 11, 38, 38, 1
 # 24, 22, 26, 26, 2
 # 36, 33, 14, 14, 3
 
-rm /tmpfs/silo-log/* -rf
-./out-perf.masstree/benchmarks/dbtest --runtime 30 --numa-memory 10G --num-threads $6 --scale-factor $6 --bench tpcc \
-    -o --workload-mix="$1,$2,$3,$4,$5,0,0,0" &> $7/silo-sf-$6-th-$6-mix-$1-$2-$3-$4-$5.txt
+$1 --runtime 30 --numa-memory 10G --num-threads $7 --scale-factor $7 --bench tpcc \
+    -o --workload-mix="$2,$3,$4,$5,$6,0,0,0" &> $8/silo-sf-$7-th-$7-mix-$2-$3-$4-$5-$6.txt
 
--- doc/silo-tpcc-results/test-tpcc-workload-mix.sh
+++ doc/silo-tpcc-results/test-tpcc-workload-mix.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-# $1 - output dir
+# $1 - silo exec
+# $2 - output dir
+
+# workload mix:
+# new order, payment, delivery, order status, stock level
+# 12, 11, 1, 38, 38
+# 24, 22, 2, 26, 26
+# 36, 33, 3, 14, 14
 
 mix_arr=(   \
-    "12 11 38 38 1" \
-    "24 22 26 26 2" \
-    "36 33 14 14 3" \
+    "12 11 1 38 38" \
+    "24 22 2 26 26" \
+    "36 33 3 14 14" \
     "45 43 4 4 4"   \
 )
 
 for t in 1 6 12 18 24; do
     for m in "${mix_arr[@]}"; do
         echo "start to run" $m sf/threads=$t
-        ./run-tpcc-workload-mix.sh $m $t $1
+        ./run-tpcc-workload-mix.sh $1 $m $t $2
     done
 done
 
