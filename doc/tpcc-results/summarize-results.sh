116a5bcd76b3c460612949641a055681d7fea4f3
Ippokratis Pandis <ipandis@cloudera.com>
2014-11-14 10:40:26 -0800

    Fixing summarize-results

--- doc/tpcc-results/summarize-results.sh
+++ doc/tpcc-results/summarize-results.sh
@@ -2,6 +2,12 @@
 # $1 silo or ermia
 # $2 results files dir
 
+if [[ $# -lt 2 ]]; then
+    echo "Too few arguments. "
+    echo "Usage $0 <result-type> <result-dir>"
+    exit
+fi
+
 mix_arr=(   \
     "12-11-1-38-38" \
     "24-22-2-26-26" \
@@ -15,9 +21,9 @@ do
     echo -ne "Mix\tCommit/s\tAborts/s\n"
     for m in "${mix_arr[@]}"; do
         echo -ne "$m\t"
-        echo -ne `tail -2 $2/$1-sf-$t-th-$t-mix-$m.txt | head -1 | cut -d ' ' -f1`
+        echo -ne `tail -2 $2/$1/silo-sf-$t-th-$t-mix-$m.txt | head -1 | cut -d ' ' -f1`
         echo -ne "\t"
-        echo -ne `tail -2 $2/$1-sf-$t-th-$t-mix-$m.txt | head -1 | cut -d ' ' -f5`
+        echo -ne `tail -2 $2/$1/silo-sf-$t-th-$t-mix-$m.txt | head -1 | cut -d ' ' -f5`
         echo -ne "\n"
     done
     echo ""
