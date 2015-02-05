7719400e6222446250a304e02ba99f79c480cfae
Tianzheng Wang <tzwang@cs.toronto.edu>
2015-02-05 06:24:30 -0500

    better script for running microbenchmarks.

--- src/run-microbench.sh
+++ src/run-microbench.sh
@@ -1,23 +1,22 @@
 #./out-perf.masstree/benchmarks/dbtest --verbose --runtime 30 --numa-memory 4G --bench tpcc --num-threads 32 --scale-factor 32 -o '--microbench --microbench-rows=100 --microbench-wr-ratio=10'
 # $1: type ("-random", random read-set or "-static", fixed read-set)
 type=$1
-EXEC="hog-machine ./out-perf.masstree/benchmarks/dbtest --verbose --runtime 30 --bench tpcc --scale-factor 32 --pin-cpu --log-dir /tmpfs/tzwang/silo-log/"
-DIR=../ermia-microbench-results-10k
-READ_ROWS=10000
-#100000
-mkdir -p $DIR
+EXEC="./out-perf.masstree/benchmarks/dbtest --verbose --runtime 30 --bench tpcc --scale-factor 32 --pin-cpu --log-dir /tmpfs/tzwang/silo-log/"
 
-#for w in 2 20 200 2000 20000
-for w in 1 10 100 1000
-#for w in 1 10 100 1000 10000
-#for w in 3 30 300 3000 30000
-do
-    for t in 6 12 18 24
-    do
-        rm -rf /tmpfs/tzwang/silo-log/*
-        export TCMALLOC_MAX_TOTAL_THREAD_CACHE_BYTES="2147483648"
-        export LD_PRELOAD="/usr/lib/libtcmalloc.so"
-        $EXEC --num-threads $t -o "--microbench$type --microbench-rows=$READ_ROWS --microbench-wr-rows=$w" &> $DIR/microbench$type-rd-$READ_ROWS-wr-$w-t-$t.txt
+export TCMALLOC_MAX_TOTAL_THREAD_CACHE_BYTES="2147483648"
+export LD_PRELOAD="/usr/lib/libtcmalloc.so"
+
+for READ_ROWS in 100000 300000; do
+    echo $READ_ROWS
+    DIR=../ermia-microbench-results-$READ_ROWS
+    mkdir -p $DIR
+    for w_ratio in 0.1 0.01 0.001 0.0001 0.00001; do
+        w=`echo $READ_ROWS \* $w_ratio | bc -l`
+        echo read $READ_ROWS rows, write $w rows
+        for t in 6 12; do
+            rm -rf /tmpfs/tzwang/silo-log/*
+            $EXEC --num-threads $t -o "--disable-read-only-snapshot --warehouse-spread=100 --microbench$type --microbench-rows=$READ_ROWS --microbench-wr-rows=$w" &> $DIR/microbench$type-rd-$READ_ROWS-wr-$w-t-$t.txt
+        done
     done
 done
 
