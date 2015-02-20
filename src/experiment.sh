bf69932dbfefb45ec11e8c0c8a5a675429a0b3c3
Kangnyeon Kim <knkim@cs.toronto.edu>
2015-02-20 15:50:16 -0500

    paper: fixed bar chart script

--- src/experiment.sh
+++ src/experiment.sh
@@ -1,10 +1,8 @@
 time=40
 
-#for bench in tpce5 tpce10 tpce20 tpce40 tpce_org tpcc_org tpcc_contention tpcc++ 
-for bench in tpce5 tpce_org tpcc_org tpcc_contention tpcc++ 
+for bench in tpce2 tpce5 tpce10 tpce20 tpce_org tpcc_org tpcc_contention tpcc++ 
 do
-#for i in 1 6 12 18 24
-for i in 18 24
+for i in 1 6 12 18 24
 do
 	./run.sh out-perf.masstree/benchmarks/dbtest $bench $i $time > ermia-$bench-$i-$time 2>&1
 done
