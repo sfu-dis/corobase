19620f7892b0257d798d7c720e6f7d350555af03
knkim <knkim@knkim@cs.toronto.edu>
2015-03-07 10:13:01 -0500

    paper: fig 1 fix( ermia vs silo), fig 5 fix( normalized)

--- doc/vldb15/figure-sources/fig-tpce-robustness.py
+++ doc/vldb15/figure-sources/fig-tpce-robustness.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from operator import truediv
 from MyData import MyData
 import matplotlib
 MyData.MyInit(matplotlib) # prep for X or no X
@@ -15,38 +16,35 @@ width = 0.20
 rcParams['figure.figsize'] = 6.9, 2.4
 matplotlib.rcParams.update({'font.size': 9})
 
-def drawBarsPayload(ax, ycol, ymax=10000, showLegend=True):
+def drawBarsPayload(ax, ycol, showLegend=True):
     xvalues=['tpce1', 'tpce5', 'tpce10', 'tpce20', 'tpce40', 'tpce60']
     plots=[]
     plotsLabel=['ERMIA-SI','ERMIA-SSI','Silo']
 
-    Xs, Ys = \
+    X1, Y1 = \
         ermia_si.filterSelect(xcol='bench', ycol=ycol, xvalues=xvalues,
                           include={'threads':24})
-    Ys[:] = [ y/40 for y in Ys]
-    print Xs, Ys
-    plots.append( ax.bar( ind + 0.2 , Ys, width, color='b'))
+    Y1[:] = [ y/40 for y in Y1]
 
-    Xs, Ys = \
+    X2, Y2 = \
         ermia_ssi.filterSelect(xcol='bench', ycol=ycol, xvalues=xvalues,
                           include={'threads':24})
-    Ys[:] = [ y/40 for y in Ys]
-    print Xs, Ys
-    plots.append( ax.bar( ind+width + 0.2, Ys, width, color='r'))
+    Y2[:] = [ y/40 for y in Y2]
 
-    Xs, Ys = \
+    X3, Y3 = \
         silo.filterSelect(xcol='bench', ycol=ycol, xvalues=xvalues,
                           include={'threads':24})
-    Ys[:] = [ y/40 for y in Ys]
-    print Xs, Ys
-    plots.append( ax.bar( ind+width+width + 0.2, Ys, width, color='y'))
+    Y3[:] = [ y/40 for y in Y3]
+
+    plots.append( ax.bar( ind + 0.2 , map(truediv, Y1, Y1), width, color='b'))
+    plots.append( ax.bar( ind+width + 0.2, map(truediv,Y2, Y1), width, color='r'))
+    plots.append( ax.bar( ind+width+width + 0.2, map(truediv,Y3,Y1), width, color='y'))
 
     ax.set_xticks( ind + 0.2+ (width*1.5) )
     ax.set_xticklabels(('1%', '5%', '10%', '20%', '40%', '60%' ), minor=False)
 
     # If shared axis then get_ylim not avail
-    ax.set_ylim(1, ymax)
-    ax.set_yscale('log', nonposy='clip')
+    ax.set_ylim(0, 1.5)
 
     # Print ratio in the title
     ax.set_xlabel('Contention degree')
@@ -56,9 +54,7 @@ def drawBarsPayload(ax, ycol, ymax=10000, showLegend=True):
         tick.label.set_fontsize(9)
 
     if showLegend:
-        # Legend locations: [upper lower center] [left right center]  or best  or center
-        #algosDisp = [algo.upper() for algo in algos]
-        ax.legend(plots, plotsLabel, 'upper right')
+        ax.legend(plots, plotsLabel, 'upper right', prop={'size':7})
         leg = ax.get_legend()
         leg.set_frame_on(False)
 
@@ -66,8 +62,8 @@ def drawBarsPayload(ax, ycol, ymax=10000, showLegend=True):
 
 f,(ax_1,ax_2) = plt.subplots(1,2,sharey=False)
 f.subplots_adjust(left=0.12, bottom=0.22, right=0.98, top=0.87, wspace=0.4)
-drawBarsPayload(ax_1, 'total_commits', 100000, False)
-drawBarsPayload(ax_2, 'total_query_commits', 100000, True)
+drawBarsPayload(ax_1, 'total_commits',  False)
+drawBarsPayload(ax_2, 'total_query_commits', True)
 ax_1.set_ylabel('Throughput (tps)', fontsize=9)
 ax_2.set_ylabel('Throughput (queries/s)', fontsize=9)
 MyData.MyShow(plt) # show or save plot
---
+++ doc/vldb_result/microbench/summarize_microbench.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# $1 - "-static" or "-random"
+# $2 - number of read rows
+
+if [ "$#" -lt 1 ]; then
+    echo "Wrong number of arguments"
+    echo "Usage $0 <-static,-random> <number of read rows>"
+    exit 0;
+fi
+type=$1
+
+r='*'
+if [ "$#" -gt 1 ]; then
+    r=$2
+fi
+#echo "Using $r"
+
+echo -ne "Reads Writes Write% Threads Commits/s Aborts/s\n"
+
+for f in `ls -1v microbench$type-rd-$r-*.txt`
+do
+  wr=`echo "$f" | cut -d '-' -f6 | awk -F '.' '{print $1}'`
+  wp=`echo "scale=5; $wr / $r * 100" | bc -l`
+  th=`echo "$f" | cut -d '-' -f8 | cut -d '.' -f1`
+  co=`tail -3 $f | head -1 | cut -d ' ' -f1`
+  ab=`tail -3 $f | head -1 | cut -d ' ' -f5`
+  ## ac=`echo "$ab / $co" | bc -l`
+  ## echo -ne "$wr $wp $th $co $ab $ac\n"
+  echo -ne "$r $wr $wp $th $co $ab\n"
+done
