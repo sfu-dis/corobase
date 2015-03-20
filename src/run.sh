237837627c214649141e2ce88ad65f048b4ba5c5
Tianzheng Wang <tzwang@cs.toronto.edu>
2015-03-20 11:10:17 -0400

    make run.sh accept additional arguments.

--- src/run.sh
+++ src/run.sh
@@ -3,6 +3,7 @@
 # $2 - benchmark
 # $3 - num of threads
 # $4 - runtime
+# $5 - other parameters like --retry-aborted-transactions
 
 if [[ $# -lt 4 ]]; then
     echo "Too few arguments. "
@@ -19,39 +20,39 @@ export LD_PRELOAD="/usr/lib/libtcmalloc.so"
 
 if [ "$2" == "tpcc_org" ]; then
 #TPCC
-numactl --interleave=all	$1 --verbose --bench tpcc --scale-factor $3  --num-threads $3 --runtime $4 --log-dir $LOGDIR --pin-cpu -o "--workload-mix="45,43,0,4,4,4,0,0,0""
+numactl --interleave=all	$1 --verbose $5 --bench tpcc --scale-factor $3  --num-threads $3 --runtime $4 --log-dir $LOGDIR --pin-cpu -o "--workload-mix="45,43,0,4,4,4,0,0,0""
 
 elif [ "$2" == "tpcc_contention" ]; then
 #TPCC
-numactl --interleave=all	$1 --verbose --bench tpcc --scale-factor $3  --num-threads $3 --runtime $4 --log-dir $LOGDIR --pin-cpu -o "--workload-mix="45,43,0,4,4,4,0,0,0" --warehouse-spread=100"
+numactl --interleave=all	$1 --verbose $5 --bench tpcc --scale-factor $3  --num-threads $3 --runtime $4 --log-dir $LOGDIR --pin-cpu -o "--workload-mix="45,43,0,4,4,4,0,0,0" --warehouse-spread=100"
 
 elif [ "$2" == "tpcc++" ]; then
 #TPCC++ ( /w credit check )
-numactl --interleave=all	$1 --verbose --bench tpcc --scale-factor $3  --num-threads $3 --runtime $4 --log-dir $LOGDIR --pin-cpu -o "--workload-mix="41,43,4,4,4,4,0,0,0" --warehouse-spread=100"
+numactl --interleave=all	$1 --verbose $5 --bench tpcc --scale-factor $3  --num-threads $3 --runtime $4 --log-dir $LOGDIR --pin-cpu -o "--workload-mix="41,43,4,4,4,4,0,0,0" --warehouse-spread=100"
 
 elif [ "$2" == "tpce_org" ]; then
-numactl --interleave=all	$1 --verbose --bench tpce --scale-factor 500 --num-threads $3 --runtime $4 --log-dir $LOGDIR --pin-cpu -o "--egen-dir ./benchmarks/egen/flat/egen_flat_in --customers 5000 --working-days 10 --workload-mix="4.9,13,1,18,14,8,10.1,10,19,2,0""
+numactl --interleave=all	$1 --verbose $5 --bench tpce --scale-factor 500 --num-threads $3 --runtime $4 --log-dir $LOGDIR --pin-cpu -o "--egen-dir ./benchmarks/egen/flat/egen_flat_in --customers 5000 --working-days 10 --workload-mix="4.9,13,1,18,14,8,10.1,10,19,2,0""
 
 elif [ "$2" == "tpce1" ]; then
-numactl --interleave=all	$1 --verbose --bench tpce --scale-factor 500 --num-threads $3 --runtime $4 --log-dir $LOGDIR --pin-cpu -o "--egen-dir ./benchmarks/egen/flat/egen_flat_in --customers 5000 --working-days 10 --query-range 1 --workload-mix="4.9,8,1,13,14,8,10.1,10,9,2,20""
+numactl --interleave=all	$1 --verbose $5 --bench tpce --scale-factor 500 --num-threads $3 --runtime $4 --log-dir $LOGDIR --pin-cpu -o "--egen-dir ./benchmarks/egen/flat/egen_flat_in --customers 5000 --working-days 10 --query-range 1 --workload-mix="4.9,8,1,13,14,8,10.1,10,9,2,20""
 
 elif [ "$2" == "tpce5" ]; then
-numactl --interleave=all	$1 --verbose --bench tpce --scale-factor 500 --num-threads $3 --runtime $4 --log-dir $LOGDIR --pin-cpu -o "--egen-dir ./benchmarks/egen/flat/egen_flat_in --customers 5000 --working-days 10 --query-range 5 --workload-mix="4.9,8,1,13,14,8,10.1,10,9,2,20""
+numactl --interleave=all	$1 --verbose $5 --bench tpce --scale-factor 500 --num-threads $3 --runtime $4 --log-dir $LOGDIR --pin-cpu -o "--egen-dir ./benchmarks/egen/flat/egen_flat_in --customers 5000 --working-days 10 --query-range 5 --workload-mix="4.9,8,1,13,14,8,10.1,10,9,2,20""
 
 elif [ "$2" == "tpce10" ]; then
-numactl --interleave=all	$1 --verbose --bench tpce --scale-factor 500 --num-threads $3 --runtime $4 --log-dir $LOGDIR --pin-cpu -o "--egen-dir ./benchmarks/egen/flat/egen_flat_in --customers 5000 --working-days 10 --query-range 10 --workload-mix="4.9,8,1,13,14,8,10.1,10,9,2,20""
+numactl --interleave=all	$1 --verbose $5 --bench tpce --scale-factor 500 --num-threads $3 --runtime $4 --log-dir $LOGDIR --pin-cpu -o "--egen-dir ./benchmarks/egen/flat/egen_flat_in --customers 5000 --working-days 10 --query-range 10 --workload-mix="4.9,8,1,13,14,8,10.1,10,9,2,20""
 
 elif [ "$2" == "tpce20" ]; then
-numactl --interleave=all	$1 --verbose --bench tpce --scale-factor 500 --num-threads $3 --runtime $4 --log-dir $LOGDIR --pin-cpu -o "--egen-dir ./benchmarks/egen/flat/egen_flat_in --customers 5000 --working-days 10 --query-range 20 --workload-mix="4.9,8,1,13,14,8,10.1,10,9,2,20""
+numactl --interleave=all	$1 --verbose $5 --bench tpce --scale-factor 500 --num-threads $3 --runtime $4 --log-dir $LOGDIR --pin-cpu -o "--egen-dir ./benchmarks/egen/flat/egen_flat_in --customers 5000 --working-days 10 --query-range 20 --workload-mix="4.9,8,1,13,14,8,10.1,10,9,2,20""
 
 elif [ "$2" == "tpce40" ]; then
-numactl --interleave=all	$1 --verbose --bench tpce --scale-factor 500 --num-threads $3 --runtime $4 --log-dir $LOGDIR --pin-cpu -o "--egen-dir ./benchmarks/egen/flat/egen_flat_in --customers 5000 --working-days 10 --query-range 40 --workload-mix="4.9,8,1,13,14,8,10.1,10,9,2,20""
+numactl --interleave=all	$1 --verbose $5 --bench tpce --scale-factor 500 --num-threads $3 --runtime $4 --log-dir $LOGDIR --pin-cpu -o "--egen-dir ./benchmarks/egen/flat/egen_flat_in --customers 5000 --working-days 10 --query-range 40 --workload-mix="4.9,8,1,13,14,8,10.1,10,9,2,20""
 
 elif [ "$2" == "tpce60" ]; then
-numactl --interleave=all	$1 --verbose --bench tpce --scale-factor 500 --num-threads $3 --runtime $4 --log-dir $LOGDIR --pin-cpu -o "--egen-dir ./benchmarks/egen/flat/egen_flat_in --customers 5000 --working-days 10 --query-range 60 --workload-mix="4.9,8,1,13,14,8,10.1,10,9,2,20""
+numactl --interleave=all	$1 --verbose $5 --bench tpce --scale-factor 500 --num-threads $3 --runtime $4 --log-dir $LOGDIR --pin-cpu -o "--egen-dir ./benchmarks/egen/flat/egen_flat_in --customers 5000 --working-days 10 --query-range 60 --workload-mix="4.9,8,1,13,14,8,10.1,10,9,2,20""
 
 elif [ "$2" == "tpce80" ]; then
-numactl --interleave=all	$1 --verbose --bench tpce --scale-factor 500 --num-threads $3 --runtime $4 --log-dir $LOGDIR --pin-cpu -o "--egen-dir ./benchmarks/egen/flat/egen_flat_in --customers 5000 --working-days 10 --query-range 80 --workload-mix="4.9,8,1,13,14,8,10.1,10,9,2,20""
+numactl --interleave=all	$1 --verbose $5 --bench tpce --scale-factor 500 --num-threads $3 --runtime $4 --log-dir $LOGDIR --pin-cpu -o "--egen-dir ./benchmarks/egen/flat/egen_flat_in --customers 5000 --working-days 10 --query-range 80 --workload-mix="4.9,8,1,13,14,8,10.1,10,9,2,20""
 
 else
 	echo "wrong bench type, check run.sh"
