5ed74e569fad6e81aa3369c1312eee44ccb54def
Tianzheng Wang <tzwang@cs.toronto.edu>
2015-11-01 17:39:32 -0800

    Larger xid context.

--- src/dbcore/xid.cpp
+++ src/dbcore/xid.cpp
@@ -52,7 +52,7 @@ namespace TXN {
    stragglers and restricts the number of concurrent epochs so that
    the problem cannot arise.
  */
-static uint16_t const NCONTEXTS = 2048;
+static uint16_t const NCONTEXTS = 8192;
 xid_context contexts[NCONTEXTS];
 
 struct bitmap {
