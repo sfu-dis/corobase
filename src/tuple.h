55c093dcd0996ed2a8090295116b16bef075c41c
Tianzheng Wang <tzwang@cs.toronto.edu>
2016-02-28 21:51:15 -0500

    Inline some tuple-related functions.

--- src/tuple.h
+++ src/tuple.h
@@ -113,12 +113,64 @@ public:
   };
 
 #if defined(USE_PARALLEL_SSN) || defined(USE_PARALLEL_SSI)
-  uint64_t age(xid_context *visitor);
+  /* return the tuple's age based on a safe_lsn provided by the calling tx.
+   * safe_lsn usually = the calling tx's begin offset.
+   *
+   * and we need to check the type of clsn because a "committed" tx might
+   * change the clsn to ASI_LOG type after changing state.
+   */
+  inline ALWAYS_INLINE uint64_t age(xid_context *visitor) {
+    uint64_t end = 0;
+    XID owner = volatile_read(visitor->owner);
+
+  retry:
+    fat_ptr cstamp = volatile_read(get_object()->_clsn);
+
+    if (cstamp.asi_type() == fat_ptr::ASI_XID) {
+      XID xid = XID::from_ptr(cstamp);
+      if (xid == owner)   // my own update
+        return 0;
+      xid_context *xc = xid_get_context(xid);
+      end = volatile_read(xc->end).offset();
+      if (not xc or xc->owner != xid)
+        goto retry;
+    }
+    else {
+      end = cstamp.offset();
+    }
+
+    // the caller must be alive...
+    return volatile_read(visitor->begin).offset() - end;
+  }
   bool is_old(xid_context *visitor);    // FOR READERS ONLY!
-  bool set_persistent_reader();
-  bool has_persistent_reader();
-  void lockout_readers();
-  void welcome_readers();
+  inline ALWAYS_INLINE bool set_persistent_reader() {
+    uint64_t pr = 0;
+    do {
+        pr = volatile_read(preader);
+        if (pr >> 7)   // some updater already locked it
+            return false;
+    }
+    while (volatile_read(preader) != PERSISTENT_READER_MARK and
+           not __sync_bool_compare_and_swap(&preader, pr, PERSISTENT_READER_MARK));
+    return true;
+  }
+  inline ALWAYS_INLINE bool has_persistent_reader() {
+    return volatile_read(preader) & PERSISTENT_READER_MARK;
+  }
+
+  // XXX: for the writer who's updating this tuple only
+  inline ALWAYS_INLINE void lockout_readers() {
+    if (not (volatile_read(preader) >> 7))
+        __sync_fetch_and_xor(&preader, uint8_t{1} << 7);
+    ASSERT(volatile_read(preader) >> 7);
+  }
+
+  // XXX: for the writer who's updating this tuple only
+  inline ALWAYS_INLINE void welcome_readers() {
+    if (volatile_read(preader) >> 7)
+        __sync_fetch_and_xor(&preader, uint64_t{1} << 7);
+    ASSERT(not (volatile_read(preader) >> 7));
+  }
 #endif
 
   inline void
