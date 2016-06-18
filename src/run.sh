f3bd175ce99230dee410602dd956ea6fc37d1c2f
Tianzheng Wang <tzwang@cs.toronto.edu>
2016-06-18 15:53:03 -0700

    Separate number of threads and sf in run.sh.

--- src/run.sh
+++ src/run.sh
@@ -19,19 +19,17 @@ trap "rm -f $LOGDIR/*" EXIT
 exe=$1
 workload=$2
 bench=${workload:0:4}
-threads=$3
-sf="0"
-runtime=$4
+sf=$3
+threads=$4
+runtime=$5
 
-if [ "$bench" == "tpcc" ]; then
-  sf=$threads
-elif [ "$bench" == "tpce" ]; then
+if [ "$bench" == "tpce" ]; then
   sf="500"
 elif [ "$bench" != "ycsb" ]; then
   echo "Unsupported benchmark $bench."
 fi
 
-options="$1 --verbose $5 --bench $bench --num-threads $threads --scale-factor $sf --runtime $runtime \
+options="$1 --verbose $6 --bench $bench --num-threads $threads --scale-factor $sf --runtime $runtime \
   --log-dir $LOGDIR --log-buffer-mb=512 --log-segment-mb=8192 --parallel-loading"
 echo $options
 if [ "$bench" == "tpcc" ]; then
@@ -42,26 +40,26 @@ if [ "$bench" == "tpcc" ]; then
     echo $suppliers_x
     suppliers=`expr $suppliers_x * 100`
     echo $suppliers
-    $options -o "--workload-mix="40,38,0,4,4,4,10,0" --suppliers=$suppliers --warehouse-spread=$wh_spread $6"
+    $options -o "--workload-mix="40,38,0,4,4,4,10,0" --suppliers=$suppliers --warehouse-spread=$wh_spread $7"
   elif [ "$btype" == "+" ]; then
-    $options -o "--workload-mix="41,43,4,4,4,4,0,0" --warehouse-spread=$wh_spread $6"
+    $options -o "--workload-mix="41,43,4,4,4,4,0,0" --warehouse-spread=$wh_spread $7"
   elif [ "$btype" == "r" ]; then
-    $options -o "--workload-mix="0,0,0,0,50,50,0,0" --warehouse-spread=$wh_spread $6"
+    $options -o "--workload-mix="0,0,0,0,50,50,0,0" --warehouse-spread=$wh_spread $7"
   else
     if [ "$workload" == "tpcc_contention" ]; then
       wh_spread="100"
     fi
-    $options -o "--workload-mix="45,43,0,4,4,4,0,0" --warehouse-spread=$wh_spread $6"
+    $options -o "--workload-mix="45,43,0,4,4,4,0,0" --warehouse-spread=$wh_spread $7"
   fi
 elif [ "$bench" == "tpce" ]; then
   if [ "$workload" == "tpce_org" ]; then
-    $options -o "--egen-dir ./benchmarks/egen/flat/egen_flat_in --customer 5000 --working-days 10 --workload-mix="4.9,13,1,18,14,8,10.1,10,19,2,0" $6"
+    $options -o "--egen-dir ./benchmarks/egen/flat/egen_flat_in --customer 5000 --working-days 10 --workload-mix="4.9,13,1,18,14,8,10.1,10,19,2,0" $7"
   else
     query_rng=${workload:4}
-    $options -o "--query-range $query_rng --egen-dir ./benchmarks/egen/flat/egen_flat_in --customer 5000 --working-days 10 --workload-mix="4.9,8,1,13,14,8,10.1,10,9,2,20" $6"
+    $options -o "--query-range $query_rng --egen-dir ./benchmarks/egen/flat/egen_flat_in --customer 5000 --working-days 10 --workload-mix="4.9,8,1,13,14,8,10.1,10,9,2,20" $7"
   fi
 elif [ "$bench" == "ycsb" ]; then
-  $options -o "$6"
+  $options -o "$7"
 else
   echo "Unspported benchmark $bench."
 fi
