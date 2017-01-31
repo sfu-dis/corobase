f8ab40535b18a864b0897e71097792ceae00504e
Tianzheng Wang <tzwang@cs.toronto.edu>
2017-01-31 12:46:29 -0800

    Skip key installation in indirection array for backups.

--- src/dbcore/sm-log-recover-impl.cpp
+++ src/dbcore/sm-log-recover-impl.cpp
@@ -65,7 +65,7 @@ sm_log_recover_impl::recover_index_insert(sm_log_scan_mgr::record_scan *logrec) 
   FID fid = logrec->fid();
   auto* fd = sm_file_mgr::get_file(fid);
   auto* oa = fd->main_array;
-  if(oa->get(logrec->oid())->key == nullptr) {
+  if(oa->get(logrec->oid())->ptr == NULL_PTR) {
     recover_index_insert(logrec, fd->index);
   }
 }
@@ -98,17 +98,22 @@ sm_log_recover_impl::recover_index_insert(sm_log_scan_mgr::record_scan *logrec, 
   ASSERT(align_up(len + sizeof(varstr)) == sz);
 
   oid_array *oa = get_impl(oidmgr)->get_array(logrec->fid());
-  if(volatile_read(oidmgr->oid_get_entry_ptr(oa, logrec->oid())->key)) {
+  if(!config::is_backup_srv() &&
+    volatile_read(oidmgr->oid_get_entry_ptr(oa, logrec->oid())->key)) {
     return;
   }
 
-  // Construct the varkey (skip the varstr struct then it's data)
-  varstr* key = (varstr*)MM::allocate(sizeof(varstr) + len, 0);
-  new (key) varstr((char *)key + sizeof(varstr), len);
-  key->copy_from((char *)payload_buf + sizeof(varstr), len);
-
-  if(index->btr.underlying_btree.insert_if_absent(*key, logrec->oid(), NULL, 0)) {
-    __sync_bool_compare_and_swap(&oidmgr->oid_get_entry_ptr(oa, logrec->oid())->key, nullptr, key);
+  varstr payload_key((char*)payload_buf + sizeof(varstr), len);
+  if(index->btr.underlying_btree.insert_if_absent(payload_key, logrec->oid(), NULL, 0)) {
+    // Don't add the key on backup - on backup chkpt will traverse OID arrays
+    if(!config::is_backup_srv()) {
+      // Construct the varkey to be inserted in the oid array
+      // (skip the varstr struct then it's data)
+      varstr* key = (varstr*)MM::allocate(sizeof(varstr) + len, 0);
+      new (key) varstr((char *)key + sizeof(varstr), len);
+      key->copy_from((char *)payload_buf + sizeof(varstr), len);
+      volatile_write(oidmgr->oid_get_entry_ptr(oa, logrec->oid())->key, key);
+    }
   }
 }
 
@@ -138,31 +143,34 @@ sm_log_recover_impl::recover_update_key(sm_log_scan_mgr::record_scan* logrec) {
   // Used when emulating the case where we didn't have OID arrays - must update tree leaf nodes
   auto* index = sm_file_mgr::get_index(logrec->fid());
   ASSERT(index);
-  auto sz = logrec->payload_size();
+  static const uint32_t kBufferSize = 128 * config::MB;
+  auto sz = align_up(logrec->payload_size());
   static __thread char *buf;
-  static __thread uint64_t buf_size;
   if (unlikely(not buf)) {
-    buf = (char *)MM::allocate(sz, 0);
-    buf_size = align_up(sz);
-  } else if (unlikely(buf_size < sz)) {
-    MM::deallocate(fat_ptr::make(buf, encode_size_aligned(buf_size)));
-    buf = (char *)MM::allocate(sz, 0);
-    buf_size = sz;
+    buf = (char *)malloc(kBufferSize);
   }
-  logrec->load_object(buf, sz);
+  char* payload_buf = nullptr;
+  ALWAYS_ASSERT(sz < kBufferSize);
+  if(logrec->payload_lsn().offset() >= logmgr->durable_flushed_lsn_offset()) {
+    // In the log buffer, point directly to it without memcpy
+    ASSERT(config::is_backup_srv());
+    auto *logrec_impl = get_impl(logrec);
+    logrec_impl->scan.has_payloads = true;  // FIXME(tzwang): do this in a better way
+    payload_buf = (char*)logrec_impl->scan.payload();
+    ALWAYS_ASSERT(payload_buf);
+  } else {
+    logrec->load_object(buf, sz);
+    payload_buf = buf;
+  }
 
   // Extract the real key length (don't use varstr.data()!)
-  size_t len = ((varstr *)buf)->size();
+  size_t len = ((varstr *)payload_buf)->size();
   ASSERT(align_up(len + sizeof(varstr)) == sz);
 
-  // Construct the varkey (skip the varstr struct then it's data)
-  varstr* key = (varstr*)MM::allocate(sizeof(varstr) + len, 0);
-  new (key) varstr((char *)key + sizeof(varstr), len);
-  key->copy_from((char *)buf + sizeof(varstr), len);
-
+  varstr key((char*)payload_buf + sizeof(varstr), len);
   OID old_oid = 0;
-  index->btr.underlying_btree.insert(*key, logrec->oid(), nullptr, &old_oid, nullptr);
-  ALWAYS_ASSERT(old_oid == logrec->oid());
+  index->btr.underlying_btree.insert(key, logrec->oid(), nullptr, &old_oid, nullptr);
+  ASSERT(old_oid == logrec->oid());
 }
 
 ndb_ordered_index*
@@ -613,8 +621,9 @@ parallel_offset_replay::redo_runner::redo_logbuf_partition() {
   auto *scan = owner->scanner->new_log_scan(start_lsn, config::eager_warm_up());
 
   for (; scan->valid() and scan->payload_lsn() < end_lsn; scan->next()) {
-    ALWAYS_ASSERT(scan->payload_lsn() >= start_lsn);
-    ALWAYS_ASSERT(scan->payload_lsn().segment() >= 1);
+    LSN payload_lsn = scan->payload_lsn();
+    //ALWAYS_ASSERT(payload_lsn >= start_lsn);
+    ALWAYS_ASSERT(payload_lsn.segment() >= 1);
     auto oid = scan->oid();
     auto fid = scan->fid();
 
