1670fa7b8be735603c02531377cefdf8a7e45847
Tianzheng Wang <tzwang@cs.toronto.edu>
2017-02-21 21:38:37 -0700

    Adapt RDMA log shipping to support multiple backups.

---
+++ src/dbcore/sm-rep-rdma.h
@@ -0,0 +1,114 @@
+#pragma once
+#include "rdma.h"
+#include "sm-rep.h"
+
+namespace rep {
+
+// Describes a node that uses RDMA for log shipping. Both the primary and
+// backups use this class to represent others and itself. The primary would
+// create multiple RdmaNodes, each encapsulates an RDMA RC connection between
+// the primary and a backup. Each backup will create exactly one RdmaNode for
+// the connecting and exchanging data with the primary.
+class RdmaNode {
+ public:
+  const static int kMessageSize = 8;
+  const static uint64_t kDaemonBufferSize = 512 * config::MB;
+
+ private:
+  char msg_buf_[kMessageSize] CACHE_ALIGNED;
+  char daemon_buf_[kDaemonBufferSize] CACHE_ALIGNED;
+  rdma::context* context_;
+
+  // Memory region indexes - these are specific and local to the underlying
+  // rdma::context; since we use a dedicated rdma::context per pair of
+  // primary-backup nodes, we can safely assume the indexes we got are
+  // correct given the primary and backups register memory regions in the
+  // same order (which is handled totally by RdmaNode).
+  uint32_t msg_buf_ridx_;
+  uint32_t log_buf_ridx_;
+  uint32_t log_buf_partition_bounds_ridx_;
+  uint32_t daemon_buf_ridx_;
+
+ public:
+  RdmaNode(bool as_primary) : context_(nullptr), msg_buf_ridx_(-1),
+    log_buf_ridx_(-1), log_buf_partition_bounds_ridx_(-1), daemon_buf_ridx_(-1) {
+    if(as_primary) {
+      context_ = new rdma::context(config::primary_port, 1);
+    } else {
+      context_ = new rdma::context(config::primary_srv, config::primary_port, 1);
+    }
+
+    memset(msg_buf_, 0, kMessageSize);
+    memset(daemon_buf_, 0, kDaemonBufferSize);
+
+    // XXX(tzawng): Assuming the backup is also registering in the same order
+    msg_buf_ridx_ = context_->register_memory(msg_buf_, kMessageSize);
+    daemon_buf_ridx_ = context_->register_memory(daemon_buf_, kDaemonBufferSize);
+
+    auto* logbuf = sm_log::get_logbuf();
+    log_buf_ridx_ = context_->register_memory(logbuf->_data, logbuf->window_size() * 2);
+    log_buf_partition_bounds_ridx_ = context_->register_memory(
+      (char*)rep::logbuf_partition_bounds, sizeof(uint64_t) * kMaxLogBufferPartitions);
+
+    context_->finish_init();
+    // TODO: send over the indexes to client
+    LOG(INFO) << "RDMA initialized (" << daemon_buf_ridx_ << ")";
+  }
+
+  ~RdmaNode() { delete context_; }
+
+  inline void WaitForMessageAsPrimary(uint64_t msg, bool reset = true) {
+    ALWAYS_ASSERT(!config::is_backup_srv());
+    while(true) {
+      uint64_t m = volatile_read(*(uint64_t *)GetMemoryRegion(msg_buf_ridx_));
+      if(m & msg) {
+        if(reset) {
+          // reset it so I'm not confused next time
+          *(uint64_t *)GetMemoryRegion(msg_buf_ridx_) = kRdmaWaiting;
+        }
+        break;
+      }
+    }
+  }
+
+  inline void PollSendCompletionAsPrimary(uint64_t nops) {
+    ALWAYS_ASSERT(!config::is_backup_srv());
+    context_->poll_send_cq(nops);
+  }
+
+  inline void SetMessageAsBackup(uint64_t msg) {
+    ALWAYS_ASSERT(config::is_backup_srv());
+    *(uint64_t *)GetMemoryRegion(msg_buf_ridx_) = msg;
+    context_->rdma_write(msg_buf_ridx_, 0, msg_buf_ridx_, 0, kMessageSize);
+  }
+
+  inline rdma::context* GetContext() { return context_; }
+
+  inline void RdmaWriteImmDaemonBuffer(uint64_t local_offset,
+    uint64_t remote_offset, uint64_t size, uint32_t imm, bool sync = true) {
+    context_->rdma_write_imm(daemon_buf_ridx_, local_offset, daemon_buf_ridx_,
+                             remote_offset, size, imm, sync);
+  }
+  inline void RdmaWriteImmLogBufferPartitionBounds(uint64_t local_offset,
+    uint64_t remote_offset, uint64_t size, uint32_t imm, bool sync = true) {
+    context_->rdma_write_imm(log_buf_partition_bounds_ridx_, local_offset,
+                             log_buf_partition_bounds_ridx_, remote_offset,
+                             size, imm, sync);
+  }
+  inline void RdmaWriteImmLogBuffer(uint64_t local_offset, uint64_t remote_offset,
+    uint64_t size, uint32_t imm, bool sync = true) {
+    context_->rdma_write_imm(log_buf_ridx_, local_offset, log_buf_ridx_,
+      remote_offset, size, imm, sync);
+  }
+  inline uint64_t ReceiveImm(uint32_t* imm = nullptr) {
+    return context_->receive_rdma_with_imm(imm);
+  }
+  inline char* GetDaemonBuffer() { return (char*)daemon_buf_; }
+
+ private:
+  inline void* GetMemoryRegion(uint32_t index) {
+    return context_->get_memory_region(index);
+  }
+};
+
+} // namespace rep
--- src/dbcore/sm-rep.h
+++ src/dbcore/sm-rep.h
@@ -30,6 +30,8 @@ struct write_record_t;
 
 namespace rep {
 
+class RdmaNode;
+
 const uint64_t kRdmaWaiting = 0x1;
 const uint64_t kRdmaReadyToReceive = 0x2;
 const uint64_t kRdmaPersisted = 0x4;
@@ -106,15 +108,12 @@ backup_start_metadata* prepare_start_metadata(int& chkpt_fd, LSN& chkpt_start_ls
 // RDMA-specific functions
 void primary_daemon_rdma();
 void start_as_backup_rdma();
-void backup_daemon_rdam();
 void primary_init_rdma();
 void primary_ship_log_buffer_rdma(const char *buf, uint32_t size,
                                   bool new_seg, uint64_t new_seg_start_offset);
-void update_pdest_on_backup_rdma(write_record_t* w);
-void send_log_files_after_rdma(backup_start_metadata* md, LSN chkpt_start);
-void rdma_wait_for_message(uint64_t msg, bool reset = true);
-void set_message(uint64_t msg);
+void send_log_files_after_rdma(RdmaNode* node, backup_start_metadata* md, LSN chkpt_start);
 void primary_rdma_poll_send_cq(uint64_t nops);
+void primary_rdma_wait_for_message(uint64_t msg, bool reset);
 //void primary_persist_remote_nvram(const char* buf, uint32_t size);
 
 // TCP-specific functions
