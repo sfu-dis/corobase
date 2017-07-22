0fb9260a3d6739820dda42ddb04d1cd8a4e1ce4e
Tianzheng Wang <tzwang@cs.toronto.edu>
2017-07-22 19:57:25 -0400

    minor.

--- src/dbcore/sm-log-alloc.h
+++ src/dbcore/sm-log-alloc.h
@@ -97,58 +97,44 @@ struct sm_log_alloc_mgr {
   bool _write_daemon_should_stop;
 
   // tzwang: use one _tls_lsn_offset per worker thread to record its
-  // most-recently
-  // committed/aborted transaction's log lsn offset. This array together is used
-  // to
-  // replace the rcu-slist as a result of the decoupling of getting an LSN and
-  // the
-  // accounting of log allocation objects (in an rcu-slist). Using the rcu-slist
-  // we
-  // have to use **at least** one CAS per transaction and there's no bounded
-  // number
-  // of steps for a thread to successfully get an LSN offset. Tests on a 32-core
-  // (4 sockets) and 60-core (4 sockets), and a 240-core (16 sockets) machines
-  // show
-  // this doesn't scale after around 30 threads.
+  // most-recently committed/aborted transaction's log lsn offset. This array
+  // together is used to replace the rcu-slist as a result of the decoupling of
+  // getting an LSN and the accounting of log allocation objects (in an
+  // rcu-slist). Using the rcu-slist we have to use **at least** one CAS per
+  // transaction and there's no bounded number of steps for a thread to
+  // successfully get an LSN offset. Tests on a 32-core (4 sockets) and 60-core
+  // (4 sockets), and a 240-core (16 sockets) machines show this doesn't scale
+  // after around 30 threads.
   //
   // So now we use a single atomic fetch_and_add to get the LSN offset. Each
-  // thread
-  // is guaranteed to get a valid LSN offset using exactly **one** instruction.
+  // thread is guaranteed to get a valid LSN offset using exactly **one**
+  // instruction.
   //
   // Still, the log flusher needs to know the "oldest live log allocation" (in
-  // the
-  // rcu-slist term), which is the earliest LSN offset/transaction that has
-  // written
-  // to the log buffer. Note the discrepancy here: in both the rcu-slist and
-  // atomic
-  // fetch-and-add schemes, we might have holes in the log buffer. So we need to
-  // find
-  // the earliest guy who has finished writing to the log buffer.
+  // the rcu-slist term), which is the earliest LSN offset/transaction that has
+  // written to the log buffer. Note the discrepancy here: in both the
+  // rcu-slist and atomic fetch-and-add schemes, we might have holes in the log
+  // buffer. So we need to find the earliest guy who has finished writing to
+  // the log buffer.
   //
   // Without a centralized rcu-slist using CAS, we have to use a fully
-  // decentralized
-  // approach - schemes like one rcu-slist per socket won't completely solve the
-  // problem because there is no coordination between threads on the same
-  // socket.
-  // That will direct us back to use CAS again which is what we wanted to avoid.
+  // decentralized approach - schemes like one rcu-slist per socket won't
+  // completely solve the problem because there is no coordination between
+  // threads on the same socket. That will direct us back to use CAS again
+  // which is what we wanted to avoid.
   //
-  // The solution is borrowed from shore-mt's dlog's passive group commit, which
-  // solves
-  // a similar problem in NVM+dlog settings. Each thread will put its just
-  // committed/aborted
-  // lsn offset (corresponds to the log allocation block in the rcu-slist
-  // scheme) to its
-  // own "tls" place, and the log flusher just scans all these tls places, then
-  // flush up
-  // to the **smallest** lsn it found.
+  // The solution is borrowed from shore-mt's dlog's passive group commit,
+  // which solves a similar problem in NVM+dlog settings. Each thread will put
+  // its just committed/aborted lsn offset (corresponds to the log allocation
+  // block in the rcu-slist scheme) to its own "tls" place, and the log flusher
+  // just scans all these tls places, then flush up to the **smallest** lsn it
+  // found.
   //
   // In case the log buffer is backed by NVRAM (ie --nvram-log-buffer == 1),
-  // the thread setting its tls_lsn_offset will clflush() then continue.
-  // So smallest_tls_lsn_offset() returns the "durable lsn" (possibly still in
-  // the
-  // log buffer). The daemon is only responsible for flushing, ie making room in
-  // the
-  // log buffer to take more transactions.
+  // the thread setting its tls_lsn_offset will clflush() then continue.  So
+  // smallest_tls_lsn_offset() returns the "durable lsn" (possibly still in the
+  // log buffer). The daemon is only responsible for flushing, ie making room
+  // in the log buffer to take more transactions.
   uint64_t *_tls_lsn_offset;
   uint64_t _lsn_offset CACHE_ALIGNED;
   uint64_t _logbuf_partition_size;
