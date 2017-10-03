8be99a244b4ab84a9c38b5ad52c58fa83e556d0e
Tianzheng Wang <tzwang@cs.toronto.edu>
2017-10-02 21:59:40 -0600

    update scripts.

--- src/run-tcp-cluster.sh
+++ src/run-tcp-cluster.sh
@@ -25,9 +25,15 @@ run() {
   nvram=$7
   persist_nvram_on_replay=$8
   persist_policy=$9
+  command_log=${10}
 
   logbuf_mb=16
-  group_commit_size_kb=4096
+
+  if [ "$command_log" == 1 ]; then
+    group_commit_size_kb=512
+  else
+    group_commit_size_kb=4096
+  fi
 
   read_view_ms=10
   read_view_stat="/dev/shm/ermia_read_view.txt"
@@ -37,36 +43,51 @@ run() {
   unset GLOG_logtostderr
 
   echo "----------"
-  echo backups:$num_backups thread:$t $policy full_redo=$full redoers=$redoers delay=$delay nvram_log_buffer=$nvram group_commit_size_kb=$group_commit_size_kb
+  echo backups:$num_backups thread:$t $policy full_redo=$full redoers=$redoers delay=$delay nvram_log_buffer=$nvram group_commit_size_kb=$group_commit_size_kb command_log=$command_log
   echo "----------"
   ./run-cluster.sh SI $t $duration $t $logbuf_mb $read_view_stat tpcc_org tpccr \
-    "-group_commit -group_commit_size_kb=$group_commit_size_kb -chkpt_interval=1000000 -node_memory_gb=19 -log_ship_by_rdma=0 -fake_log_write -wait_for_backups -num_backups=$num_backups -persist_policy=$persist_policy -read_view_stat_interval_ms=$read_view_ms -read_view_stat_file=$read_view_stat" \
-    "-primary_host=$primary -node_memory_gb=20 -log_ship_by_rdma=0 -nvram_log_buffer=$nvram -quick_bench_start -wait_for_primary -replay_policy=$policy -full_replay=$full -replay_threads=$redoers -nvram_delay_type=$delay -read_view_stat_interval_ms=$read_view_ms -read_view_stat_file=$read_view_stat" \
+    "-group_commit -group_commit_size_kb=$group_commit_size_kb -chkpt_interval=1000000 -node_memory_gb=19 -log_ship_by_rdma=0 -fake_log_write -wait_for_backups -num_backups=$num_backups -persist_policy=$persist_policy -read_view_stat_interval_ms=$read_view_ms -read_view_stat_file=$read_view_stat -command_log=$command_log -command_log_buffer_mb=16" \
+    "-primary_host=$primary -node_memory_gb=20 -log_ship_by_rdma=0 -nvram_log_buffer=$nvram -quick_bench_start -wait_for_primary -replay_policy=$policy -full_replay=$full -replay_threads=$redoers -nvram_delay_type=$delay -read_view_stat_interval_ms=$read_view_ms -read_view_stat_file=$read_view_stat -command_log=$command_log -command_log_buffer_mb=16" \
     "${backups[@]:0:$num_backups}"
   echo
 }
 
+sync_clock() {
+  sudo ntpd -gq &> /dev/null
+  for b in $backups; do
+    ssh -o StrictHostKeyChecking=no $b "sudo ntpd -gq &> /dev/null"
+  done
+}
+
 # Synchronous shipping
 sync_ship() {
-  # Sync time
+  sync_clock
   for num_backups in 1 2 3 4 5 6 7; do
-    sudo ntpd -gq &> /dev/null
-    for redoers in 1 2 4 8 16; do
-      run $num_backups 16 sync 1 $redoers none 0 0 sync
-      run $num_backups 16 "bg" 1 $redoers none 0 0 sync
+    for redoers in 1 8 16; do
+      #run $num_backups 16 sync 1 $redoers none 0 0 sync 0
+      run $num_backups 16 "bg" 1 $redoers none 0 0 sync 0
     done
-    run $num_backups 16 none 1 0 none 0 0 sync
+    run $num_backups 16 none 1 0 none 0 0 sync 0
   done
 }
 
 async_ship() {
-  # Sync time
+  sync_clock
   for num_backups in 1 2 3 4 5 6 7; do
-    sudo ntpd -gq &> /dev/null
-    for redoers in 1 2 4 8 16; do
-      run $num_backups 16 "bg" 1 $redoers none 0 0 async
+    for redoers in 1 8 16; do
+      run $num_backups 16 "bg" 1 $redoers none 0 0 async 0
     done
-    run $num_backups 16 none 1 0 none 0 0 async
+    run $num_backups 16 none 1 0 none 0 0 async 0
+  done
+}
+
+cmdlog() {
+  sync_clock
+  for num_backups in 1 2 3 4 5 6 7; do
+    run $num_backups 16 none 1 0 none 0 0 sync 1
+    for redoers in 1 8 16; do
+      run $num_backups 16 "bg" 1 $redoers none 0 0 sync 1
+    done
   done
 }
 
@@ -76,5 +97,8 @@ for r in 1 2 3; do
 
   echo "Running sync_ship r$r"
   sync_ship
+
+  echo "Running cmdlog r$r"
+  cmdlog
 done
 
