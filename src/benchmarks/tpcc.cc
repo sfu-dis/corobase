6515a2fec4c4f21668bcf791d4509ac43c5c3c4d
Tianzheng Wang <tzwang@cs.toronto.edu>
2018-08-05 19:18:15 -0400

    Boilerplate for tpcc-dora variant (no actual functionality yet).

--- src/benchmarks/tpcc.cc
+++ src/benchmarks/tpcc.cc
@@ -21,92 +21,10 @@
 #include "bench.h"
 #include "tpcc.h"
 
-typedef std::vector<std::vector<std::pair<int32_t, int32_t>>> SuppStockMap;
-SuppStockMap supp_stock_map(10000);  // value ranges 0 ~ 9999 ( modulo by 10k )
-
-struct eqstr {
-  bool operator()(const char *s1, const char *s2) const {
-    return (s1 == s2) || (s1 && s2 && strcmp(s1, s2) == 0);
-  }
-};
-
 #define TPCC_TABLE_LIST(x)                                                     \
   x(customer) x(customer_name_idx) x(district) x(history) x(item) x(new_order) \
       x(oorder) x(oorder_c_id_idx) x(order_line) x(stock) x(stock_data)        \
           x(nation) x(region) x(supplier) x(warehouse)
-
-struct Nation {
-  int id;
-  std::string name;
-  int rId;
-};
-
-const Nation nations[] = {{48, "ALGERIA", 0},
-                          {49, "ARGENTINA", 1},
-                          {50, "BRAZIL", 1},
-                          {51, "CANADA", 1},
-                          {52, "EGYPT", 4},
-                          {53, "ETHIOPIA", 0},
-                          {54, "FRANCE", 3},
-                          {55, "GERMANY", 3},
-                          {56, "INDIA", 2},
-                          {57, "INDONESIA", 2},
-
-                          {65, "IRAN", 4},
-                          {66, "IRAQ", 4},
-                          {67, "JAPAN", 2},
-                          {68, "JORDAN", 4},
-                          {69, "KENYA", 0},
-                          {70, "MOROCCO", 0},
-                          {71, "MOZAMBIQUE", 0},
-                          {72, "PERU", 1},
-                          {73, "CHINA", 2},
-                          {74, "ROMANIA", 3},
-                          {75, "SAUDI ARABIA", 4},
-                          {76, "VIETNAM", 2},
-                          {77, "RUSSIA", 3},
-                          {78, "UNITED KINGDOM", 3},
-                          {79, "UNITED STATES", 1},
-                          {80, "CHINA", 2},
-                          {81, "PAKISTAN", 2},
-                          {82, "BANGLADESH", 2},
-                          {83, "MEXICO", 1},
-                          {84, "PHILIPPINES", 2},
-                          {85, "THAILAND", 2},
-                          {86, "ITALY", 3},
-                          {87, "SOUTH AFRICA", 0},
-                          {88, "SOUTH KOREA", 2},
-                          {89, "COLOMBIA", 1},
-                          {90, "SPAIN", 3},
-
-                          {97, "UKRAINE", 3},
-                          {98, "POLAND", 3},
-                          {99, "SUDAN", 0},
-                          {100, "UZBEKISTAN", 2},
-                          {101, "MALAYSIA", 2},
-                          {102, "VENEZUELA", 1},
-                          {103, "NEPAL", 2},
-                          {104, "AFGHANISTAN", 2},
-                          {105, "NORTH KOREA", 2},
-                          {106, "TAIWAN", 2},
-                          {107, "GHANA", 0},
-                          {108, "IVORY COAST", 0},
-                          {109, "SYRIA", 4},
-                          {110, "MADAGASCAR", 0},
-                          {111, "CAMEROON", 0},
-                          {112, "SRI LANKA", 2},
-                          {113, "ROMANIA", 3},
-                          {114, "NETHERLANDS", 3},
-                          {115, "CAMBODIA", 2},
-                          {116, "BELGIUM", 3},
-                          {117, "GREECE", 3},
-                          {118, "PORTUGAL", 3},
-                          {119, "ISRAEL", 4},
-                          {120, "FINLAND", 3},
-                          {121, "SINGAPORE", 2},
-                          {122, "NORWAY", 3}};
-
-const char *regions[] = {"AFRICA", "AMERICA", "ASIA", "EUROPE", "MIDDLE EAST"};
 
 class tpcc_table_scanner : public ermia::OrderedIndex::ScanCallback {
  public:
