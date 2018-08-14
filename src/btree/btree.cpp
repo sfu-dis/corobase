72c7da966671d0fee89aa9c836eae35cb63c208b
Tianzheng Wang <tzwang@sfu.ca>
2018-08-13 21:44:27 -0700

    WIP: First single-threaded btree implementation with working insert in leaf and internal nodes. Split, traversal, etc. to do and test.

---
+++ src/btree/btree.cpp
@@ -0,0 +1,180 @@
+#include <cassert>
+#include <glog/logging.h>
+#include "btree.h"
+
+namespace ermia {
+namespace btree {
+
+template<uint32_t NodeSize, class PayloadType>
+bool LeafNode<NodeSize, PayloadType>::Add(char *key,
+                                          uint32_t key_size,
+                                          PayloadType &payload,
+                                          Stack &stack) {
+  // Find the position in the leaf entry array which begins at data_
+  // FIXME(tzwang): do binary search here
+  uint32_t insert_idx = 0;
+  for (insert_idx = 0; insert_idx < num_keys_; ++insert_idx) {
+    NodeEntry &entry = GetEntry(insert_idx);
+    int cmp = entry.CompareKey(key, key_size);
+    if (cmp == 0) {
+      // Key already exists
+      return false;
+    } else if (cmp > 0) {
+      // Found the place
+      break;
+    }
+  }
+
+  // Check space
+  if (key_size + sizeof(payload) + data_size_ + sizeof(LeafNode) > Capacity()) {
+    abort();
+    // Need split
+    auto *right = Split(stack);
+    if (insert_idx > num_keys_ / 2) {  // Belongs to the new right sibling
+      bool inserted = right->Add(key, key_size, payload, stack);
+      LOG_IF(FATAL, !inserted);
+      return inserted;
+    }
+  }
+
+  InsertAt(insert_idx, key, key_size, payload);
+  return true;
+}
+
+template<uint32_t NodeSize, class PayloadType>
+void LeafNode<NodeSize, PayloadType>::InsertAt(uint32_t idx,
+                                               char *key,
+                                               uint32_t key_size,
+                                               PayloadType &payload) {
+  // Found the place to insert: move everything after the insert location
+  memmove(&((NodeEntry *)data_)[idx + 1],
+          &((NodeEntry *)data_)[idx],
+          sizeof(NodeEntry) * (num_keys_ - idx));
+
+  // Now the idx-th slot is ready
+  NodeEntry *entry = (NodeEntry *)data_ + idx;
+  char *data_start = data_ + NodeSize - sizeof(*this) - data_size_ - key_size - sizeof(payload);
+  new (entry) NodeEntry(key_size, sizeof(payload), data_start, key, (char*)&payload);
+  assert(memcmp(entry->GetKeyData(), key, key_size) == 0);
+  assert(memcmp(entry->GetValueData(), &payload, sizeof(payload)) == 0);
+
+  ++num_keys_;
+  data_size_ += (key_size + sizeof(payload));
+}
+
+template<uint32_t NodeSize, class PayloadType>
+LeafNode<NodeSize, PayloadType>* LeafNode<NodeSize, PayloadType>::Split(Stack &stack) {
+  LOG_IF(FATAL, num_keys_ < 2);
+  LeafNode<NodeSize, PayloadType> *right_sibling = LeafNode::New();
+  // The last half of keys/values go to the right sibling
+  uint32_t keys_to_move = num_keys_ / 2;
+  for (uint32_t i = num_keys_ - keys_to_move; i < num_keys_; ++i) {
+    NodeEntry &entry = GetEntry(i);
+    bool added = right_sibling->Add(entry.GetKeyData(), entry.GetKeySize(),
+                                    *(PayloadType *)entry.GetValueData(), stack);
+    LOG_IF(FATAL, !added) << "Couldn't add key-value";
+  }
+
+  // Keys >= this separator are on the right sibling
+  NodeEntry &entry = GetEntry(num_keys_ - keys_to_move);
+
+  // Insert the separator key to parent
+  InternalNode<NodeSize> *parent = (InternalNode<NodeSize> *)stack.Top()->node;
+  parent->Add(entry.GetKeyData(), entry.GetKeySize(), this, right_sibling, stack);
+
+  // Reduce the number of keys, leave the data there to be overwritten later
+  num_keys_ -= keys_to_move;
+
+  return right_sibling;
+}
+
+template<uint32_t NodeSize>
+void InternalNode<NodeSize>::Add(char *key, uint32_t key_size,
+                                 Node *left_child, Node *right_child,
+                                 Stack &stack) {
+  // Find the position in the leaf entry array which begins at data_
+  // FIXME(tzwang): do binary search here
+  uint32_t insert_idx = 0;
+  for (insert_idx = 0; insert_idx < num_keys_; ++insert_idx) {
+    NodeEntry &entry = GetEntry(insert_idx);
+    int cmp = entry.CompareKey(key, key_size);
+    LOG_IF(FATAL, cmp == 0) << "Key already exists in parent node";
+    if (cmp > 0) {
+      // Found the place
+      break;
+    }
+  }
+
+  // Check space
+  if (key_size + sizeof(right_child) + data_size_ + sizeof(InternalNode) > Capacity()) {
+    abort();
+    // Need split
+    auto *right = Split(stack);
+    if (insert_idx > num_keys_ / 2) {  // Belongs to the new right sibling
+      right->Add(key, key_size, left_child, right_child, stack);
+    }
+  }
+
+  InsertAt(insert_idx, key, key_size, left_child, right_child);
+}
+
+template<uint32_t NodeSize>
+InternalNode<NodeSize> *InternalNode<NodeSize>::Split(Stack &stack) {
+  LOG_IF(FATAL, num_keys_ < 2);
+  InternalNode<NodeSize> *right_sibling = InternalNode::New();
+  // The last half of keys/values go to the right sibling
+  uint32_t keys_to_move = num_keys_ / 2;
+  for (uint32_t i = num_keys_ - keys_to_move; i < num_keys_; ++i) {
+    NodeEntry &entry = GetEntry(i);
+    right_sibling->Add(entry.GetKeyData(), entry.GetKeySize(),
+                       (Node *)GetEntry(i - 1).GetValueData(),
+                       (Node *)entry.GetValueData(), stack);
+  }
+
+  // Keys >= this separator are on the right sibling
+  NodeEntry &entry = GetEntry(num_keys_ - keys_to_move);
+
+  // Insert the separator key to parent
+  InternalNode<NodeSize> *parent = (InternalNode<NodeSize> *)stack.Top()->node;
+  parent->Add(entry.GetKeyData(), entry.GetKeySize(), this, right_sibling, stack);
+
+  // Reduce the number of keys, leave the data there to be overwritten later
+  num_keys_ -= keys_to_move;
+
+  return right_sibling;
+}
+
+template<uint32_t NodeSize>
+void InternalNode<NodeSize>::InsertAt(uint32_t idx,
+                                      char *key, uint32_t key_size,
+                                      Node *left_child, Node *right_child) {
+  // Found the place to insert: move everything after the insert location
+  memmove(&((NodeEntry *)data_)[idx + 1],
+          &((NodeEntry *)data_)[idx],
+          sizeof(NodeEntry) * (num_keys_ - idx));
+
+  // Now the idx-th slot is ready
+  NodeEntry &entry = GetEntry(idx);
+
+  // right_child goes to the value space 
+  char *data_start = data_ + NodeSize - sizeof(*this) - data_size_ - key_size - sizeof(Node*);
+  new (&entry) NodeEntry(key_size, sizeof(Node*), data_start, key, (char*)&right_child);
+  assert(memcmp(entry.GetKeyData(), key, key_size) == 0);
+  assert(memcmp(entry.GetValueData(), (char *)&right_child, sizeof(Node*)) == 0);
+
+  ++num_keys_;
+  data_size_ += (key_size + sizeof(Node*));
+
+  // Set up left sibling's right child pointer
+  if (idx == 0) {
+    min_ptr_ = left_child;
+  } else {
+    NodeEntry &left_sibling = GetEntry(idx - 1);
+    memcpy(left_sibling.GetValueData(), (char *)&left_child, sizeof(InternalNode*));
+  }
+}
+
+// Template instantiation
+template class LeafNode<4096, int>;
+}  // namespace btree
+}  // namespace ermia
---
+++ src/btree/btree.h
@@ -0,0 +1,122 @@
+#pragma once
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+namespace ermia {
+namespace btree {
+
+class Node {
+protected:
+  uint32_t num_keys_;
+
+public:
+  Node() : num_keys_(0) {}
+  inline uint32_t NumKeys() { return num_keys_; }
+};
+
+struct Stack {
+  struct Frame {
+    Node *node;
+    Frame() : node(nullptr) {}
+    Frame(Node *node) : node(node) {}
+    ~Frame() { node = nullptr; }
+  };
+
+  static const uint32_t kMaxFrames = 32;
+  Frame frames[kMaxFrames];
+  uint32_t num_frames;
+
+  Stack() : num_frames(0) {}
+  ~Stack() { num_frames = 0; }
+  inline void Push(Node *node) { new (&frames[num_frames++]) Frame(node); }
+  inline Frame *Pop() { return &frames[--num_frames]; }
+  Frame *Top() { return &frames[num_frames - 1]; }
+};
+
+class NodeEntry {
+private:
+  uint32_t key_size_;    // Key size
+  uint32_t value_size_;  // Value size
+  char *data_;           // Data (includes key and value) address
+
+private:
+  inline char *GetData() { return data_; }
+  static int Compare(char *d1, uint32_t l1, char *d2, uint32_t l2) {
+    int cmp = memcmp(d1, d2, std::min<uint32_t>(l1, l2));
+    if (cmp == 0 && l1 != l2) {
+      return l1 > l2 ? 1 : -1;
+    }
+    return cmp;
+  }
+
+public:
+  NodeEntry() : key_size_(0), value_size_(0), data_(nullptr) {}
+  NodeEntry(uint32_t key_size, uint32_t value_size, char *data, char *key, char *value)
+    : key_size_(key_size), value_size_(value_size), data_(data) {
+    memcpy(data_, key, key_size_);
+    memcpy(data_ + key_size_, value, value_size_);
+  }
+  inline uint32_t GetKeySize() { return key_size_; }
+  inline uint32_t GetValueSize() { return value_size_; }
+  inline char *GetKeyData() { return data_; }
+  inline char *GetValueData() { return data_ + key_size_; }
+
+  inline int CompareKey(char *key, uint32_t size) {
+    return NodeEntry::Compare(GetKeyData(), key_size_, key, size);
+  }
+};
+
+template<uint32_t NodeSize, class PayloadType>
+class LeafNode : public Node {
+private:
+  uint32_t data_size_;  // Includes keys and values
+  char data_[0];  // Must be the last element
+
+private:
+  void InsertAt(uint32_t idx, char *key, uint32_t key_size, PayloadType &payload);
+  // Split out a new right sibling, self as the left sibling
+  LeafNode *Split(Stack &stack);
+  inline NodeEntry &GetEntry(uint32_t idx) { return ((NodeEntry *)data_)[idx]; }
+
+public:
+  LeafNode() : Node(), data_size_(0) {}
+
+  static LeafNode *New() {
+    LeafNode *node = (LeafNode *)malloc(NodeSize);
+    new (node) LeafNode();
+    return node;
+  }
+
+  // Data area size, including keys and values
+  inline uint32_t Capacity() { return NodeSize - sizeof(*this); }
+  inline char *GetKey(uint32_t idx) { return GetEntry(idx).GetKeyData(); }
+  inline char *GetValue(uint32_t idx) { return GetEntry(idx).GetValueData(); }
+  bool Add(char *key, uint32_t key_size, PayloadType &payload, Stack &stack);
+};
+
+template<uint32_t NodeSize>
+class InternalNode : public Node {
+private:
+  Node *min_ptr_;
+  uint32_t data_size_;  // Includes keys only, pointers are stored in InternalEntries
+  char data_[0];  // Must be the last element
+
+private:
+  void InsertAt(uint32_t idx, char *key, uint32_t key_size, Node *left_child, Node *right_child);
+  InternalNode *Split(Stack &stack);
+
+public:
+  InternalNode() : min_ptr_(nullptr), data_size_(0) {}
+  inline uint32_t Capacity() { return NodeSize - sizeof(*this); }
+  inline NodeEntry &GetEntry(uint32_t idx) { return ((NodeEntry *)data_)[idx]; }
+  static inline InternalNode *New() {
+    InternalNode<NodeSize> *node = (InternalNode *)malloc(NodeSize);
+    new (node) InternalNode<NodeSize>;
+    return node;
+  }
+  void Add(char *key, uint32_t key_size, Node *left_child, Node *right_child, Stack &stack);
+  Node *MinPtr() { return min_ptr_; }
+};
+}  // namespace btree
+}  // namespace ermia
