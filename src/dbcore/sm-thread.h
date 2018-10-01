dccd9aedc6d307d1370e786f54cd574caa387f00
Tianzheng Wang <tzwang@sfu.ca>
2018-09-30 18:20:33 -0700

    Add a ermia::thread primitive to return all threads on a physical core. Need testing.

--- src/dbcore/sm-thread.h
+++ src/dbcore/sm-thread.h
@@ -94,7 +94,8 @@ struct PerNodeThreadPool {
   Thread *threads CACHE_ALIGNED;
   uint64_t bitmap CACHE_ALIGNED;  // max 64 threads per node, 1 - busy, 0 - free
 
-  inline Thread *GetThread(bool physical = true) {
+  // Get a single new thread which can be physical or logical
+  inline Thread *GetThread(bool physical) {
   retry:
     uint64_t b = volatile_read(bitmap);
     uint64_t xor_pos = b ^ (~uint64_t{0});
@@ -105,7 +106,6 @@ struct PerNodeThreadPool {
 
     Thread *t = threads + pos;
     // Find the thread that matches the preferred type 
-    bool found = (t->is_physical != physical);
     while (true) {
       ++pos;
       if (pos >= max_threads_per_node) {
@@ -124,6 +124,60 @@ struct PerNodeThreadPool {
     return t;
   }
 
+  // Get a thread group - which includes all the threads (phyiscal + logical) on
+  // the same phyiscal core. Similar to GetThread, but continue to also allocate
+  // the logical threads that follow immediately the physical thread in the
+  // bitmap.
+  inline bool GetThreadGroup(std::vector<Thread*> &thread_group) {
+  retry:
+    thread_group.clear();
+    uint64_t b = volatile_read(bitmap);
+    uint64_t xor_pos = b ^ (~uint64_t{0});
+    uint64_t pos = __builtin_ctzll(xor_pos);
+    if (pos == max_threads_per_node) {
+      return false;
+    }
+
+    Thread *t = threads + pos;
+    // Find the thread that matches the preferred type 
+    while (true) {
+      ++pos;
+      if (pos >= max_threads_per_node) {
+        return false;
+      }
+      t = &threads[pos];
+      if ((!((1UL << pos) & b)) && t->is_physical) {
+        break;
+      }
+    }
+
+    thread_group.push_back(t);
+
+    // Got the physical thread, now try to claim the logical ones as well
+    uint64_t count = 1;  // Number of 1-bits, including the physical thread
+    ++pos;
+    while (true) {
+      t = threads + pos;
+      if (t->is_physical) {
+        break;
+      } else {
+        thread_group.push_back(t);
+        ++count;
+      }
+    }
+
+    // Fill [count] bits starting from [pos]
+    uint64_t bits = 0;
+    for (uint32_t i = pos; i < pos + count; ++i) {
+      bits |= (1UL << pos);
+    }
+    if (not __sync_bool_compare_and_swap(&bitmap, b, b | bits)) {
+      goto retry;
+    }
+    ALWAYS_ASSERT(pos < max_threads_per_node);
+    return true;
+  }
+
   inline void PutThread(Thread *t) {
     auto b = ~uint64_t{1UL << (t - threads)};
     __sync_fetch_and_and(&bitmap, b);
@@ -134,18 +188,32 @@ struct PerNodeThreadPool {
 
 extern PerNodeThreadPool *thread_pools;
 
-inline Thread *GetThread(uint16_t from) {
-  return thread_pools[from].GetThread();
+inline Thread *GetThread(uint16_t from, bool physical = true) {
+  return thread_pools[from].GetThread(physical);
 }
 
-inline Thread *GetThread(/* don't care where */) {
+inline Thread *GetThread(bool physical = true /* don't care where */) {
   for (uint16_t i = 0; i < config::numa_nodes; i++) {
-    auto *t = thread_pools[i].GetThread();
+    auto *t = thread_pools[i].GetThread(physical);
     if (t) {
       return t;
     }
   }
   return nullptr;
+}
+
+// Return all the threads (physical + logical) on the same physical core
+inline bool GetThreadGroup(uint16_t from, std::vector<Thread*> &threads) {
+  return thread_pools[from].GetThreadGroup(threads);
+}
+
+inline bool GetThreadGroup(std::vector<Thread*> &threads /* don't care where */) {
+  for (uint16_t i = 0; i < config::numa_nodes; i++) {
+    if (thread_pools[i].GetThreadGroup(threads)) {
+      break;
+    }
+  }
+  return threads.size() > 0;
 }
 
 inline void PutThread(Thread *t) { thread_pools[t->node].PutThread(t); }
