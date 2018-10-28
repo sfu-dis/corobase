34eb455df8319644e9747809fde02b5a08d0046e
Tianzheng Wang <tzwang@sfu.ca>
2018-10-28 15:33:11 -0700

    Move some thread pool functions to sm-thread.cpp.

--- src/dbcore/sm-thread.h
+++ src/dbcore/sm-thread.h
@@ -94,96 +94,22 @@ struct PerNodeThreadPool {
   Thread *threads CACHE_ALIGNED;
   uint64_t bitmap CACHE_ALIGNED;  // max 64 threads per node, 1 - busy, 0 - free
 
+  PerNodeThreadPool(uint16_t n);
+
   // Get a single new thread which can be physical or logical
-  inline Thread *GetThread(bool physical) {
-  retry:
-    uint64_t b = volatile_read(bitmap);
-    uint64_t xor_pos = b ^ (~uint64_t{0});
-    uint64_t pos = __builtin_ctzll(xor_pos);
-    if (pos == max_threads_per_node) {
-      return nullptr;
-    }
-
-    Thread *t = threads + pos;
-    // Find the thread that matches the preferred type 
-    while (true) {
-      ++pos;
-      if (pos >= max_threads_per_node) {
-        return nullptr;
-      }
-      t = &threads[pos];
-      if ((!((1UL << pos) & b)) && (t->is_physical == physical)) {
-        break;
-      }
-    }
-
-    if (not __sync_bool_compare_and_swap(&bitmap, b, b | (1UL << pos))) {
-      goto retry;
-    }
-    ALWAYS_ASSERT(pos < max_threads_per_node);
-    return t;
-  }
+  Thread *GetThread(bool physical);
 
   // Get a thread group - which includes all the threads (phyiscal + logical) on
   // the same phyiscal core. Similar to GetThread, but continue to also allocate
   // the logical threads that follow immediately the physical thread in the
   // bitmap.
-  inline bool GetThreadGroup(std::vector<Thread*> &thread_group) {
-  retry:
-    thread_group.clear();
-    uint64_t b = volatile_read(bitmap);
-    uint64_t xor_pos = b ^ (~uint64_t{0});
-    uint64_t pos = __builtin_ctzll(xor_pos);
-    if (pos == max_threads_per_node) {
-      return false;
-    }
+  bool GetThreadGroup(std::vector<Thread*> &thread_group);
 
-    Thread *t = threads + pos;
-    // Find the thread that matches the preferred type 
-    while (true) {
-      ++pos;
-      if (pos >= max_threads_per_node) {
-        return false;
-      }
-      t = &threads[pos];
-      if ((!((1UL << pos) & b)) && t->is_physical) {
-        break;
-      }
-    }
-
-    thread_group.push_back(t);
-
-    // Got the physical thread, now try to claim the logical ones as well
-    uint64_t count = 1;  // Number of 1-bits, including the physical thread
-    ++pos;
-    while (true) {
-      t = threads + pos;
-      if (t->is_physical) {
-        break;
-      } else {
-        thread_group.push_back(t);
-        ++count;
-      }
-    }
-
-    // Fill [count] bits starting from [pos]
-    uint64_t bits = 0;
-    for (uint32_t i = pos; i < pos + count; ++i) {
-      bits |= (1UL << pos);
-    }
-    if (not __sync_bool_compare_and_swap(&bitmap, b, b | bits)) {
-      goto retry;
-    }
-    ALWAYS_ASSERT(pos < max_threads_per_node);
-    return true;
-  }
-
+  // Release a thread back to the pool
   inline void PutThread(Thread *t) {
     auto b = ~uint64_t{1UL << (t - threads)};
     __sync_fetch_and_and(&bitmap, b);
   }
-
-  PerNodeThreadPool(uint16_t n);
 };
 
 extern PerNodeThreadPool *thread_pools;
