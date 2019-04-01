26a342b808147793326b0a15c97fe0ef698ef8b6
Tianzheng Wang <tzwang@sfu.ca>
2019-03-31 23:09:41 -0700

    Avoid new/delete unlocked_tcursor in AMAC.

--- src/masstree/masstree_tcursor.hh
+++ src/masstree/masstree_tcursor.hh
@@ -52,6 +52,8 @@ class unlocked_tcursor {
         lv_(leafvalue<P>::make_empty()),
         root_(table.fix_root()) {}
 
+  inline unlocked_tcursor() {}
+
   bool find_unlocked(threadinfo& ti);
   // a coroutine variant of find_unlocked
   ermia::dia::generator<bool> coro_find_unlocked(threadinfo& ti);
