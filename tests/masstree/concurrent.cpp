ac6b8789ed5efd4bd4f8492d9d47919feafe46bd
Jiacheng Lu <lujc.me@gmail.com>
2019-10-18 16:29:07 -0700

    Complete simple concurrent insertion test

--- tests/masstree/concurrent.cpp
+++ tests/masstree/concurrent.cpp
@@ -34,22 +34,19 @@ void mock_destroy_xid_context(ermia::TXN::xid_context *xid_context) {
 class ConcurrentMasstree: public ::testing::Test {
    protected:
     virtual void SetUp() override {
-        ermia::config::node_memory_gb = 5;
+        ermia::config::node_memory_gb = 2;
         ermia::config::num_backups = 0;
-        ermia::config::threads = 32;
+        ermia::config::numa_spread = false;
+        ermia::config::threads = 2;
 
         ermia::config::init();
         ermia::MM::prepare_node_memory();
-
-        initThreadsPool();
 
         tree_ = new ermia::ConcurrentMasstree();
     }
 
     virtual void TearDown() override {
         delete tree_;
-
-        finiThreadsPool();
     }
 
     PROMISE(bool) insertRecord(const Record &record) {
@@ -79,7 +76,7 @@ class ConcurrentMasstree: public ::testing::Test {
         RETURN res;
     }
 
-    void initThreadsPool() {
+    void initRunningThreads() {
         for(uint32_t i = 0; i < ermia::config::threads; i++) {
             ermia::thread::Thread* th = ermia::thread::GetThread(true);
             runnable_threads_.emplace_back(th);
@@ -87,7 +84,7 @@ class ConcurrentMasstree: public ::testing::Test {
 
     }
 
-    void finiThreadsPool() {
+    void finiRunningThreads() {
         for(ermia::thread::Thread * th : runnable_threads_) {
             th->Join();
             th->Destroy();
@@ -99,10 +96,9 @@ class ConcurrentMasstree: public ::testing::Test {
 };
 
 TEST_F(ConcurrentMasstree, InsertSequential) {
-    const std::vector<Record> all_records = genRandRecords(200, 128);
-
-    const size_t per_thread_records = all_records.size() / runnable_threads_.size();
-
+    const size_t per_thread_records = 10;
+    const size_t all_records_num = per_thread_records * runnable_threads_.size();
+    const std::vector<Record> all_records = genRandRecords(all_records_num, 128);
 
     ermia::thread::Thread::Task insert_task = [&] (char *beg) {
         size_t begin_index = static_cast<size_t>(reinterpret_cast<uint64_t>(beg));
@@ -114,55 +110,20 @@ TEST_F(ConcurrentMasstree, InsertSequential) {
         }
     };
 
+    initRunningThreads();
+
     size_t thread_begin_idx = 0;
     for(ermia::thread::Thread * th : runnable_threads_) {
+        // hack: pass a int by casting the pointer value
         th->StartTask(insert_task, reinterpret_cast<char *>(thread_begin_idx));
         thread_begin_idx += per_thread_records;
     }
+
+    finiRunningThreads();
 }
 
 #ifdef USE_STATIC_COROUTINE
 
-//TEST_F(ConcurrentMasstree, InsertInterleaved) {
-//    std::vector<Record> records_to_insert = genRandRecords(50, 40);
-//
-//    constexpr int task_queue_size= 5;
-//    std::array<task<bool>, task_queue_size> task_queue;
-//    uint32_t next_task_index = 0;
-//    for (task<bool> & coro_task : task_queue) {
-//        const Record & record = records_to_insert[next_task_index];
-//        next_task_index++;
-//        coro_task = insertRecord(record);
-//    }
-//
-//    uint32_t completed_task_cnt = 0;
-//    while (completed_task_cnt < records_to_insert.size()) {
-//        for(task<bool> & coro_task : task_queue) {
-//            if(!coro_task.valid()) {
-//                continue;
-//            }
-//
-//            if(!coro_task.done()) {
-//                coro_task.resume();
-//            } else {
-//                EXPECT_TRUE(coro_task.get_return_value());
-//                completed_task_cnt++;
-//
-//                if(next_task_index < records_to_insert.size()) {
-//                    const Record & record = records_to_insert[next_task_index];
-//                    next_task_index++;
-//                    coro_task = insertRecord(record);
-//                } else {
-//                    coro_task = task<bool>(nullptr);
-//                }
-//            }
-//        }
-//    }
-//
-//    for(task<bool> & coro_task : task_queue) {
-//        EXPECT_TRUE(!coro_task.valid() || coro_task.done());
-//    }
-//
-//}
 
 #endif
+
