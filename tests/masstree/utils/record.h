6265c1f5762b0c33a067f13fc2e0d98a4a2fbc97
Jiacheng Lu <lujc.me@gmail.com>
2019-10-21 14:16:54 -0700

    Polish concurrent masstree testing code

--- tests/masstree/utils/record.h
+++ tests/masstree/utils/record.h
@@ -5,13 +5,21 @@
 #include <vector>
 
 struct Record {
+    Record() : key(""), value(0) {}
+    Record(const std::string & k, ermia::OID v) : key(k), value(v) {}
     std::string key;
     ermia::OID value;
 };
 
-std::vector<Record> genRandRecords(uint32_t record_num, uint32_t key_len_avg);
+std::vector<Record> genRandRecords(uint32_t record_num,
+                                   uint32_t key_len_avg=128);
 
-std::string genKeyNotInRecords(const std::vector<Record>& records);
+std::vector<Record> genDisjointRecords(const std::vector<Record>& ref_records,
+                                       uint32_t record_num,
+                                       uint32_t key_len_avg=128);
+
+std::string genKeyNotInRecords(const std::vector<Record>& records,
+                               uint32_t key_len_avg=128);
 
 std::vector<Record> recordsSearchRange(const std::vector<Record>& records,
                                        const std::string& beg,
