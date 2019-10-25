52c8ffe8bbb841d5f5e075c72f3cb9f045f6aacd
Jiacheng Lu <lujc.me@gmail.com>
2019-10-25 13:38:24 -0700

    Add perf tests for Amac, GeneratorCoro and TaskCoro
    - Still needs to figure out what's the best batch size of coroutine
    executiion.
    - TaskCoro is actually a little more efficient than GeneratorCoro, which
    is somewhat a good news.

--- tests/masstree/perf_single_threaded.cpp
+++ tests/masstree/perf_single_threaded.cpp
@@ -19,6 +19,9 @@ using ermia::MM::node_memory;
 template <typename T>
 using task = ermia::dia::task<T>;
 
+using generator_handle = std::experimental::coroutine_handle<
+    ermia::dia::generator<bool>::promise_type>;
+
 class PerfSingleThreadSearch : public benchmark::Fixture {
   public:
     void SetUp(const ::benchmark::State &state) {
@@ -32,7 +35,7 @@ class PerfSingleThreadSearch : public benchmark::Fixture {
         node_memory = new char *[ermia::config::numa_nodes];
 
         // pre-allocate memory on node for current thread node
-        ermia::config::node_memory_gb = 1;
+        ermia::config::node_memory_gb = 2;
         allocated_node_memory[node] = 0;
 
         numa_set_preferred(node);
@@ -56,7 +59,7 @@ class PerfSingleThreadSearch : public benchmark::Fixture {
 
         for(const Record & record : records) {
             sync_wait_coro(
-                AWAIT tree_->insert(
+                tree_->insert(
                     ermia::varstr(record.key.data(), record.key.size()),
                     record.value,
                     &context_mock, nullptr, nullptr));
@@ -81,7 +84,46 @@ class PerfSingleThreadSearch : public benchmark::Fixture {
 
 #ifdef USE_STATIC_COROUTINE
 
+BENCHMARK_DEFINE_F(PerfSingleThreadSearch, AdvancedCoro) (benchmark::State &st) {
+    for (auto _ : st) {
+        constexpr uint32_t queue_size = 3; // TODO move to benchmark::State 
+
+        constexpr ermia::epoch_num cur_epoch = 0;
+
+        std::vector<task<bool>> task_queue(queue_size);
+        std::vector<ermia::OID> out_values(queue_size);
+
+        uint32_t completed_task_cnt = 0;
+        uint32_t next_record_idx = 0;
+        while (completed_task_cnt < records.size()) {
+            for(uint32_t i= 0; i < queue_size; i++) {
+                task<bool> & coro_task = task_queue[i];
+                if(coro_task.valid() ){
+                    if(coro_task.done()) {
+                        coro_task.resume();
+                    } else {
+                        completed_task_cnt++;
+                        coro_task = task<bool>(nullptr);
+                    }
+                }
+
+                if(!coro_task.valid() && next_record_idx < records.size()) {
+                    const Record & record = records[next_record_idx++];
+                    coro_task = tree_->search(
+                            ermia::varstr(record.key.data(), record.key.size()),
+                            out_values[i], cur_epoch, nullptr);
+                    // initial suspended
+                    coro_task.resume();
+                }
+            }
+        }
+    }    
+}
+
+BENCHMARK_REGISTER_F(PerfSingleThreadSearch, AdvancedCoro)->Range(8<<4, 8<<10);
+
 #else
+
 BENCHMARK_DEFINE_F(PerfSingleThreadSearch, Sequential) (benchmark::State &st) {
     for (auto _ : st) {
         constexpr ermia::epoch_num cur_epoch = 0;
@@ -93,6 +135,83 @@ BENCHMARK_DEFINE_F(PerfSingleThreadSearch, Sequential) (benchmark::State &st) {
     }
 }
 
-BENCHMARK_REGISTER_F(PerfSingleThreadSearch, Sequential)->Range(8, 8<<10);
+BENCHMARK_REGISTER_F(PerfSingleThreadSearch, Sequential)->Range(8<<4, 8<<10);
+
+BENCHMARK_DEFINE_F(PerfSingleThreadSearch, SimpleCoro) (benchmark::State &st) {
+    for (auto _ : st) {
+        constexpr uint32_t queue_size = 3; // TODO move to benchmark::State 
+
+        std::vector<generator_handle> generator_queue(queue_size, nullptr);
+        std::vector<ermia::OID> out_values(queue_size, 0);
+
+        constexpr ermia::epoch_num cur_epoch = 0;
+        ermia::SingleThreadedMasstree::threadinfo ti(cur_epoch);
+
+        uint32_t completed_search_num = 0;
+        uint32_t next_record_idx = 0;
+
+        while(completed_search_num < records.size()) {
+            for(uint32_t i = 0; i < queue_size; i++) {
+                generator_handle & handle = generator_queue[i];
+
+                if (handle) {
+                    if(!handle.done()) {
+                        handle.resume();
+                    } else {
+                        completed_search_num++;
+                        handle.destroy();
+                        handle = nullptr;
+                    }
+                }
+                
+                if(!handle && next_record_idx < records.size()) {
+                    const Record & record = records[next_record_idx++];
+                    handle = tree_->search_coro(
+                        ermia::varstr(record.key.data(), record.key.size()),
+                        out_values[i], ti, nullptr).get_handle();
+                }
+            }
+        }
+    }
+}
+
+BENCHMARK_REGISTER_F(PerfSingleThreadSearch, SimpleCoro)->Range(8<<4, 8<<10);
+
+BENCHMARK_DEFINE_F(PerfSingleThreadSearch, Amac) (benchmark::State &st) {
+    for (auto _ : st) {
+        constexpr uint32_t parellel_amac_num = 3;
+
+        constexpr ermia::epoch_num cur_epoch = 0;
+
+        std::vector<ermia::SingleThreadedMasstree::AMACState> amac_states;
+        std::vector<ermia::varstr> amac_params;
+        amac_states.reserve(parellel_amac_num);
+        amac_params.reserve(parellel_amac_num);
+
+        uint32_t next_record_idx = 0;
+        while(next_record_idx < records.size()) {
+            for(uint32_t i = 0; i < parellel_amac_num; i++) {
+                if (next_record_idx >= records.size()) {
+                    break;
+                }
+
+                const Record & record = records[next_record_idx++];
+                amac_params.emplace_back(record.key.data(), record.key.size());
+                amac_states.emplace_back(&amac_params.back());
+            }
+
+            if(amac_states.empty()) {
+                break;
+            }
+
+            tree_->search_amac(amac_states, cur_epoch);
+
+            amac_states.clear();
+            amac_params.clear();
+        }
+    }
+}
+
+BENCHMARK_REGISTER_F(PerfSingleThreadSearch, Amac)->Range(8<<4, 8<<10);
 
 #endif
