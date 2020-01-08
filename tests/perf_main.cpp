27f9f30664fbcb69356e454cfabff769320e758d
Jiacheng Lu <lujc.me@gmail.com>
2020-01-07 17:53:51 -0800

    Runtime configurable perf

--- tests/perf_main.cpp
+++ tests/perf_main.cpp
@@ -1,8 +1,12 @@
+#include <gflags/gflags.h>
 #include <benchmark/benchmark.h>
 
 #include <dbcore/sm-alloc.h>
 #include <dbcore/sm-config.h>
 #include <dbcore/sm-coroutine.h>
+
+DEFINE_bool(enable_perf, false, "enable linux perf");
+DEFINE_string(perf_record_event, "", "linux perf event, empty means show all");
 
 void ermia_init() {
     ermia::config::node_memory_gb = 10;
@@ -13,16 +17,31 @@ void ermia_init() {
     ermia::config::physical_workers_only = false;
     ermia::config::threads = 4;
 
-    // Enable profiling
-    ermia::config::enable_perf = true;
-    ermia::config::perf_record_event = "cycles";
-
     ermia::config::init();
     ermia::MM::prepare_node_memory();
 }
 
 int main(int argc, char** argv) {
-    ::benchmark::Initialize(&argc, argv);
+    google::ParseCommandLineFlags(&argc, &argv, true);
+
+    if (FLAGS_enable_perf) {
+        std::vector<char *> nargv;
+        for (uint32_t i = 0; i < argc; i++) {
+            nargv.emplace_back(argv[i]);
+        }
+
+        // ensure each perf runs only 1 iteration
+        char* ss = "--benchmark_min_time=0"; 
+        nargv.emplace_back(ss);
+
+        ermia::config::enable_perf = FLAGS_enable_perf;
+        ermia::config::perf_record_event = FLAGS_perf_record_event;
+
+        int nargc = nargv.size();
+        ::benchmark::Initialize(&nargc, nargv.data());
+    } else {
+        ::benchmark::Initialize(&argc, argv);
+    }
 
     ermia_init();
     // FIXME:
