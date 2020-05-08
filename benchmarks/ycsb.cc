cf338dffaacec93c1c4244aa4ab081744d2792af
Jiacheng Lu <jla565@sfu.ca>
2020-05-08 00:21:14 -0700

    Support iterator scan workload in ycsb with n level coroutine execution.

--- benchmarks/ycsb.cc
+++ benchmarks/ycsb.cc
@@ -47,7 +47,14 @@ class ycsb_sequential_worker : public ycsb_base_worker {
 
     if (ycsb_workload.scan_percent()) {
       if (g_read_txn_type == ReadTransactionType::Sequential) {
-        w.push_back(workload_desc("Scan", double(ycsb_workload.scan_percent()) / 100.0, TxnScan));
+        if (ermia::config::scan_with_it) {
+            w.push_back(workload_desc(
+                "Scan", double(ycsb_workload.scan_percent()) / 100.0, TxnScanWithIterator));
+        } else {
+            w.push_back(workload_desc(
+                "ScanWithIterator",
+                double(ycsb_workload.scan_percent()) / 100.0, TxnScan));
+        }
       } else {
         LOG(FATAL) << "Scan txn type must be sequential";
       }
@@ -61,6 +68,7 @@ class ycsb_sequential_worker : public ycsb_base_worker {
   static rc_t TxnReadSimpleCoroMultiGet(bench_worker *w) { return static_cast<ycsb_sequential_worker *>(w)->txn_read_simple_coro_multiget(); }
   static rc_t TxnRMW(bench_worker *w) { return static_cast<ycsb_sequential_worker *>(w)->txn_rmw(); }
   static rc_t TxnScan(bench_worker *w) { return static_cast<ycsb_sequential_worker *>(w)->txn_scan(); }
+  static rc_t TxnScanWithIterator(bench_worker *w) { return static_cast<ycsb_sequential_worker *>(w)->txn_scan_with_iterator(); }
 
   // Read transaction using traditional sequential execution
   rc_t txn_read() {
@@ -223,13 +231,49 @@ class ycsb_sequential_worker : public ycsb_base_worker {
       ermia::transaction *txn = db->NewTransaction(0, *arena, txn_buf());
       rc_t rc = rc_t{RC_INVALID};
       for (uint i = 0; i < g_reps_per_tx; ++i) {
-          ScanRange range  = GenerateScanRange(txn);
+          ScanRange range = GenerateScanRange(txn);
           if (ermia::config::index_probe_only) {
               ycsb_scan_oid_callback callback;
               table_index->ScanOID(txn, range.start_key, &range.end_key, rc, callback);
           } else {
               ycsb_scan_callback callback;
               rc = table_index->Scan(txn, range.start_key, &range.end_key, callback);
+          }
+
+#if defined(SSI) || defined(SSN) || defined(MVOCC)
+          TryCatch(rc);  // Might abort if we use SSI/SSN/MVOCC
+#else
+          // TODO(lujc): sometimes return RC_FALSE, no value?
+          // ALWAYS_ASSERT(rc._val == RC_TRUE);
+#endif
+      }
+      TryCatch(db->Commit(txn));
+      return {RC_TRUE};
+  }
+
+  rc_t txn_scan_with_iterator() {
+      ermia::transaction *txn = db->NewTransaction(0, *arena, txn_buf());
+      rc_t rc = rc_t{RC_INVALID};
+      for (uint i = 0; i < g_reps_per_tx; ++i) {
+          ScanRange range = GenerateScanRange(txn);
+          ycsb_scan_callback callback;
+          ermia::varstr valptr;
+          auto iter = ermia::ConcurrentMasstree::ScanIterator<
+              /*IsRerverse=*/false>::factory(&table_index->GetMasstree(),
+                                             txn->GetXIDContext(),
+                                             range.start_key, &range.end_key);
+          bool more = iter.init_or_next</*IsNext=*/false>();
+          while (more) {
+              if (!ermia::config::index_probe_only) {
+                  if (iter.tuple()) {
+                      rc = txn->DoTupleRead(iter.tuple(), &valptr);
+                      if (rc._val == RC_TRUE) {
+                          callback.Invoke(iter.key().data(), iter.key().length(),
+                                          valptr);
+                      }
+                  }
+              }
+              more = iter.init_or_next</*IsNext=*/true>();
           }
 
 #if defined(SSI) || defined(SSN) || defined(MVOCC)
